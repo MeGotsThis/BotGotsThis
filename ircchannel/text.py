6ea5971497ebbea83fb9472683b13673b221f5d5
MeGotsThis <megotsthis@gmail.com>
2014-11-10 01:00:27 -0800

    Recoded the text module

--- ircchannel/text.py
+++ ircchannel/text.py
@@ -7,42 +7,38 @@ import ircbot.irc
 def customCommands(channelThread, nick, message, msgParts, permissions):
     command = msgParts[0]
     channel = channelThread.channel
-    if permissions['owner']:
-        owner = textOwner.owner
-        if channel in owner and command in owner[channel]:
-            channelThread.sendMessage(
-                owner[channel][command].replace('\n', ''))
-            return True
-        if command in owner['global']:
-            channelThread.sendMessage(
-                owner['global'][command].replace('\n', ''))
-            return True
-    if permissions['broadcaster']:
-        broadcaster = textBroadcaster.broadcaster
-        if channel in broadcaster and command in broadcaster[channel]:
-            channelThread.sendMessage(
-                               broadcaster[channel][command].replace('\n', ''))
-            return True
-        if command in broadcaster['global']:
-            channelThread.sendMessage(
-                broadcaster['global'][command].replace('\n', ''))
-            return True
+    message = None
+    
+    public = textPublic.public
+    if command in public['global']:
+        message = public['global'][command].replace('\n', '')
+    if channel in public and command in public[channel]:
+        message = public[channel][command].replace('\n', '')
+    
     if permissions['moderator']:
         mods = textMods.mods
-        if channel in mods and command in mods[channel]:
-            channelThread.sendMessage(
-                               mods[channel][command].replace('\n', ''))
-            return True
         if command in mods['global']:
-            channelThread.sendMessage(
-                mods['global'][command].replace('\n', ''))
-            return True
-    public = textPublic.public
-    if channel in public and command in public[channel]:
-        channelThread.sendMessage(public[channel][command].replace('\n', ''))
+            message = mods['global'][command].replace('\n', '')
+        if channel in mods and command in mods[channel]:
+            message = mods[channel][command].replace('\n', '')
+    
+    if permissions['broadcaster']:
+        broadcaster = textBroadcaster.broadcaster
+        if command in broadcaster['global']:
+            message = broadcaster['global'][command].replace('\n', '')
+        if channel in broadcaster and command in broadcaster[channel]:
+            message = broadcaster[channel][command].replace('\n', '')
+    
+    if permissions['owner']:
+        owner = textOwner.owner
+        if command in owner['global']:
+            message = owner['global'][command].replace('\n', '')
+        if channel in owner and command in owner[channel]:
+            message = owner[channel][command].replace('\n', '')
+    
+    if message:
+        channelThread.sendMessage(message)
         return True
-    if command in public['global']:
-        channelThread.sendMessage(public['global'][command].replace('\n', ''))
-        return True
+    
     return False
 
