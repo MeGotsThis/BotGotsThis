3930137a7bda88f0fb3682ec531cb3e280bb702b
MeGotsThis <megotsthis@gmail.com>
2015-01-14 01:14:30 -0800

    Added exception file recording for all the parsed messages

--- ircchannel/commands.py
+++ ircchannel/commands.py
@@ -5,8 +5,10 @@ import ircchannel.text
 import ircuser.jtv
 import ircbot.irc
 import threading
+import traceback
 import datetime
 import time
+import sys
 
 # Set up our commands function
 def parse(channelData, nick, message):
@@ -15,6 +17,7 @@ def parse(channelData, nick, message):
     
     if nick == 'jtv':
         ircuser.jtv.parse(channelData, message)
+        return
     
     msgParts = message.split(None)
     if len(msgParts) == 0:
@@ -31,58 +34,74 @@ def threadParse(channelData, nick, message, msgParts):
         nick = str()
         message = str()
         msgParts = [str(), str()]
-
-    if config.owner is not None:
-        isOwner = nick == config.owner.lower()
-        _ = channelData.channel == '#' + config.botnick
-        isOwnerChan = channelData.channel == '#' + config.owner or _
-    else:
-        isOwner = False
-        isOwnerChan = False
-    isStaff = isOwner or nick in channelData.twitchStaff
-    isAdmin = isStaff or nick in channelData.twitchAdmin
-    isGlobalMod = isAdmin or nick in channelData.globalMods
-    isBroadcaster = isGlobalMod or isAdmin or '#' + nick == channelData.channel
-    isMod = isBroadcaster or nick in channelData.mods
-    isSubscriber = isBroadcaster or nick in channelData.subscribers
-    isTurbo = isBroadcaster or nick in channelData.turboUsers
-    isChanMod = config.botnick in channelData.mods
-    permissions = {
-        'owner': isOwner,
-        'ownerChan': isOwnerChan,
-        'staff': isStaff,
-        'admin': isAdmin,
-        'globalMod': isGlobalMod,
-        'broadcaster': isBroadcaster,
-        'moderator': isMod,
-        'subscriber': isMod,
-        'turbo': isMod,
-        'channelModerator': isChanMod,
-        }
     
-    command = str(msgParts[0]).lower()
+    try:
+        if config.owner is not None:
+            isOwner = nick == config.owner.lower()
+            _ = channelData.channel == '#' + config.botnick
+            isOwnerChan = channelData.channel == '#' + config.owner or _
+        else:
+            isOwner = False
+            isOwnerChan = False
+        isStaff = isOwner or nick in channelData.twitchStaff
+        isAdmin = isStaff or nick in channelData.twitchAdmin
+        isGlobalMod = isAdmin or nick in channelData.globalMods
+        isBroadcaster = '#' + nick == channelData.channel
+        isBroadcaster = isGlobalMod or isAdmin or isBroadcaster
+        isMod = isBroadcaster or nick in channelData.mods
+        isSubscriber = isBroadcaster or nick in channelData.subscribers
+        isTurbo = isBroadcaster or nick in channelData.turboUsers
+        isChanMod = config.botnick in channelData.mods
+        permissions = {
+            'owner': isOwner,
+            'ownerChan': isOwnerChan,
+            'staff': isStaff,
+            'admin': isAdmin,
+            'globalMod': isGlobalMod,
+            'broadcaster': isBroadcaster,
+            'moderator': isMod,
+            'subscriber': isMod,
+            'turbo': isMod,
+            'channelModerator': isChanMod,
+            }
     
-    complete = False
-    arguments = channelData, nick, message, msgParts, permissions
-    if command in ircchannel.commandList.commands:
-        commandInfo = ircchannel.commandList.commands[command]
-        hasPermission = True
-        if commandInfo[1] is not None:
-            permissionSet = commandInfo[1].split('+')
-            for perm in permissionSet:
-                hasPermission = hasPermission and permissions[perm]
-        if hasPermission and commandInfo[0] is not None:
-            complete = commandInfo[0](*arguments)
-    if not complete:
-        for comm in ircchannel.commandList.commandsStartWith:
-            if command.startswith(comm):
-                commandInfo = ircchannel.commandList.commandsStartWith[comm]
-                hasPermission = True
-                if commandInfo[1] is not None:
-                    permissionSet = commandInfo[1].split('+')
-                    for perm in permissionSet:
-                        hasPermission = hasPermission and permissions[perm]
-                if hasPermission and commandInfo[0] is not None:
-                    complete = commandInfo[0](*arguments)
-    if not complete:
-        ircchannel.text.customCommands(*arguments)
+        command = str(msgParts[0]).lower()
+    
+        complete = False
+        arguments = channelData, nick, message, msgParts, permissions
+        if command in ircchannel.commandList.commands:
+            commInfo = ircchannel.commandList.commands[command]
+            hasPerm = True
+            if commInfo[1] is not None:
+                permissionSet = commInfo[1].split('+')
+                for perm in permissionSet:
+                    hasPerm = hasPerm and permissions[perm]
+            if hasPerm and commInfo[0] is not None:
+                complete = commInfo[0](*arguments)
+        if not complete:
+            for comm in ircchannel.commandList.commandsStartWith:
+                if command.startswith(comm):
+                    commInfo = ircchannel.commandList.commandsStartWith[comm]
+                    hasPerm = True
+                    if commInfo[1] is not None:
+                        permissionSet = commInfo[1].split('+')
+                        for perm in permissionSet:
+                            hasPerm = hasPerm and permissions[perm]
+                    if hasPerm and commInfo[0] is not None:
+                        complete = commInfo[0](*arguments)
+                        if complete:
+                            break
+        if not complete:
+            ircchannel.text.customCommands(*arguments)
+    except Exception as e:
+        now = datetime.datetime.now()
+        _ = traceback.format_exception(*sys.exc_info())
+        if config.exceptionLog is not None:
+            with open(config.exceptionLog, 'a', encoding='utf-8') as file:
+                file.write(now.strftime('%Y-%m-%d %H:%M:%S.%f '))
+                file.write('Exception in thread ')
+                file.write(threading.current_thread().name + ':\n')
+                file.write('Channel: ' + channelData.channel + '\n')
+                file.write('Message: ' + message + '\n')
+                file.write('Nick: ' + nick + '\n')
+                file.write(''.join(_))
