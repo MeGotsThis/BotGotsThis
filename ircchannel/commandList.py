1d513c5a82c9dc2042e14c8370ed215059b0be11
MeGotsThis <megotsthis@gmail.com>
2015-05-23 00:07:18 -0700

    Added !autorepeat

--- ircchannel/commandList.py
+++ ircchannel/commandList.py
@@ -7,6 +7,7 @@ import ircchannel.pyramid
 import ircchannel.wall
 import ircchannel.mod
 import ircchannel.text
+import ircchannel.repeat
 try:
     import privatechannel.commandList as commandList
 except:
@@ -32,6 +33,7 @@ commands = {
     '!leave': (ircchannel.broadcaster.commandLeave, 'broadcaster'),
     '!feature': (ircchannel.feature.commandFeature, 'broadcaster'),
     '!empty': (ircchannel.broadcaster.commandEmpty, 'broadcaster'),
+    '!autorepeat': (ircchannel.repeat.commandAutoRepeat, 'broadcaster'),
     '!pyramid': (ircchannel.pyramid.commandPyramid, 'moderator'),
     '!rpyramid': (ircchannel.pyramid.commandRPyramid, 'moderator'),
     '!wall': (ircchannel.wall.commandWall, 'moderator'),
@@ -56,6 +58,7 @@ commands = {
 commandsStartWith = {
     '!pyramid-': (ircchannel.pyramid.commandPyramidLong, 'moderator'),
     '!wall-': (ircchannel.wall.commandWallLong, 'moderator'),
+    '!autorepeat-': (ircchannel.repeat.commandAutoRepeat, 'broadcaster'),
 }
 
 commands = dict(list(commands.items()) + list(commandList.commands.items()))
---
+++ ircchannel/repeat.py
@@ -0,0 +1,89 @@
+import threading
+import datetime
+import time
+import json
+
+def commandAutoRepeat(channelData, nick, message, msgParts, permissions):
+    """
+    !autorepeat 1 MONEY MONEY
+    !autorepeat-20 0.5 MONEY MONEY 
+    !autorepeat-20 0
+    !autorepeat-20 off
+    """
+
+    msgParts = msgParts = message.split(None, 2)
+    if '-' in msgParts[0]:
+        try:
+            count = int(msgParts[0].split('-')[1])
+        except:
+            count = 10
+    else:
+        count = None
+    try:
+        if msgParts[1].lower() == 'off':
+            minutesDuration = 0
+        else:
+            minutesDuration = float(msgParts[1])
+    except Exception:
+        return False
+    
+    try:
+        messageToSend = msgParts[2]
+    except IndexError:
+        messageToSend = None
+    
+    if 'repeatThread' in channelData.sessionData:
+        channelData.sessionData['repeatThread'].count = 0
+    
+    if minutesDuration <= 0 or count == 0 or not messageToSend:
+        return True
+    
+    thread = MessageRepeater(
+        channelData=channelData,
+        message=messageToSend,
+        duration=datetime.timedelta(minutes=minutesDuration),
+        count=count)
+    channelData.sessionData['repeatThread'] = thread
+    thread.start()
+    
+    return True
+
+class MessageRepeater(threading.Thread):
+    def __init__(self, *args,
+                 channelData, message='',
+                 duration=datetime.timedelta(), count=None):
+        threading.Thread.__init__(self, *args)
+        self._channelData = channelData
+        self._message = message
+        self._count = count
+        self._duration = max(duration, datetime.timedelta(seconds=1))
+        self._lastTime = datetime.datetime.min
+        self._countLock = threading.Lock()
+    
+    @property
+    def count(self):
+        with self._countLock:
+            return self._count
+    
+    @count.setter
+    def count(self, value):
+        with self._countLock:
+            self._count = value
+    
+    def run(self):
+        while self._continueRunning():
+            if datetime.datetime.now() >= self._lastTime + self._duration:
+                self._lastTime = datetime.datetime.now()
+                self._channelData.sendMessage(self._message)
+                with self._countLock:
+                    if self._count is not None:
+                        self._count -= 1
+            time.sleep(1/20)
+        if ('repeatThread' in self._channelData.sessionData
+            and self._channelData.sessionData['repeatThread'] is self):
+            del self._channelData.sessionData['repeatThread']
+    
+    def _continueRunning(self):
+        with self._countLock:
+            return self._count is None or self._count > 0
+    
