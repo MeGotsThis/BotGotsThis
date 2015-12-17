2bb1e4183369f0e0452fe0e65856622221adb948
MeGotsThis <megotsthis@gmail.com>
2015-12-16 21:55:06 -0800

    Fixed list chats so it sends it in multiple lines due to character limits

--- source/public/manage/listchats.py
+++ source/public/manage/listchats.py
@@ -1,6 +1,25 @@
-﻿from bot import globals
+﻿from bot import config, globals
 
 def manageListChats(db, send, nick, message, msgParts):
     channels = [c for c in globals.channels.keys()]
-    send('Twitch Chats: ' + ', '.join(channels))
+    channels.sort()
+    chanList = []
+    length = 0
+    while channels:
+        chan = channels.pop(0)
+        chanList.append(chan)
+        if length:
+            length += 2
+        length += len(chan)
+        if length >= config.messageLimit:
+            if len(chanList) > 1:
+                send('Twitch Chats: ' + ', '.join(chanList[:-1]))
+                del chanList[:-1]
+                length = len(chan)
+            else:
+                send('Twitch Chats: ' + ', '.join(chanList))
+                chanList.clear()
+                length = 0
+    if chanList:
+        send('Twitch Chats: ' + ', '.join(chanList))
     return True
