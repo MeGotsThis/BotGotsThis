61883e1510eef09ad6c214fefeef8ae700331306
MeGotsThis <megotsthis@gmail.com>
2016-02-25 15:00:03 -0800

    Added a timestamp column to the timeout logs

--- source/database/sqlite/timeout.sql
+++ source/database/sqlite/timeout.sql
@@ -1,5 +1,6 @@
 CREATE TABLE timeout_logs (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    theTimestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     broadcaster VARCHAR NOT NULL,
     twitchUser VARCHAR NOT NULL,
     fromUser VARCHAR,
