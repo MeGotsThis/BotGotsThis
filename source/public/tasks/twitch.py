a6005037142b46a53fc06aa12f9994b14c03b08f
MeGotsThis <megotsthis@gmail.com>
2016-04-06 01:09:06 -0700

    Added an catch exception for gaierror

--- source/public/tasks/twitch.py
+++ source/public/tasks/twitch.py
@@ -5,43 +5,47 @@ import copy
 import datetime
 import json
 import random
+import socket
 
 def checkStreamsAndChannel(timestamp):
     if not globals.channels:
         return
-    channels = copy.copy(globals.channels)
-    channelsList = ','.join([c for c in globals.channels])
-    uri = '/kraken/streams?channel=' + channelsList
-    response, responseData = twitch.twitchCall(None, 'GET', uri)
-    onlineStreams = []
-    if response.status == 200:
-        streamsData = json.loads(responseData.decode('utf-8'))
-        for stream in streamsData['streams']:
-            channel = stream['channel']['name'].lower()
-            params = stream['created_at'], '%Y-%m-%dT%H:%M:%SZ',
-            streamingSince = datetime.datetime.strptime(*params)
-            twitchStatus = stream['channel']['status']
-            twitchGame = stream['channel']['game']
+    try:
+        channels = copy.copy(globals.channels)
+        channelsList = ','.join([c for c in globals.channels])
+        uri = '/kraken/streams?channel=' + channelsList
+        response, responseData = twitch.twitchCall(None, 'GET', uri)
+        onlineStreams = []
+        if response.status == 200:
+            streamsData = json.loads(responseData.decode('utf-8'))
+            for stream in streamsData['streams']:
+                channel = stream['channel']['name'].lower()
+                params = stream['created_at'], '%Y-%m-%dT%H:%M:%SZ',
+                streamingSince = datetime.datetime.strptime(*params)
+                twitchStatus = stream['channel']['status']
+                twitchGame = stream['channel']['game']
+                channelData = channels[channel]
+                channelData.streamingSince = streamingSince
+                channelData.twitchStatus = twitchStatus
+                channelData.twitchGame = twitchGame
+                onlineStreams.append(channel)
+        
+        for channel in channels:
+            if channel in onlineStreams:
+                continue
+            uri = '/kraken/channels/' + channel
+            response, responseData = twitch.twitchCall(None, 'GET', uri)
+            if response.status != 200:
+                continue
+            stream = json.loads(responseData.decode('utf-8'))
+            twitchStatus = stream['status']
+            twitchGame = stream['game']
             channelData = channels[channel]
-            channelData.streamingSince = streamingSince
+            channelData.streamingSince = None
             channelData.twitchStatus = twitchStatus
             channelData.twitchGame = twitchGame
-            onlineStreams.append(channel)
-    
-    for channel in channels:
-        if channel in onlineStreams:
-            continue
-        uri = '/kraken/channels/' + channel
-        response, responseData = twitch.twitchCall(None, 'GET', uri)
-        if response.status != 200:
-            continue
-        stream = json.loads(responseData.decode('utf-8'))
-        twitchStatus = stream['status']
-        twitchGame = stream['game']
-        channelData = channels[channel]
-        channelData.streamingSince = None
-        channelData.twitchStatus = twitchStatus
-        channelData.twitchGame = twitchGame
+    except socket.gaierror:
+        pass
 
 def checkChatServers(timestamp):
     cooldown = datetime.timedelta(seconds=3600)
