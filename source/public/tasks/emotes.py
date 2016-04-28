c1be8bbd5a26cc6ac91d445a687f6168e323bd0d
MeGotsThis <megotsthis@gmail.com>
2016-04-27 21:37:55 -0700

    Redone loading emotes for channels

--- source/public/tasks/emotes.py
+++ source/public/tasks/emotes.py
@@ -2,7 +2,9 @@
 from ...api import ffz
 from ...api import twitch
 from bot import globals
+import copy
 import datetime
+import random
 
 def refreshTwitchGlobalEmotes(timestamp):
     since = timestamp - globals.globalEmotesCache
@@ -10,18 +12,33 @@ def refreshTwitchGlobalEmotes(timestamp):
         twitch.updateTwitchEmotes()
 
 def refreshFrankerFaceZEmotes(timestamp):
+    cooldown = datetime.timedelta(hours=1)
     since = timestamp - globals.globalFfzEmotesCache
-    if since >= datetime.timedelta(hours=1):
+    if since >= cooldown:
         ffz.updateGlobalEmotes()
-    for chan in globals.channels:
-        since = timestamp - globals.channels[chan].ffzCache
-        if since >= datetime.timedelta(hours=1):
-            globals.channels[chan].updateFfzEmotes()
-            return
+    channels = copy.copy(globals.channels)
+    toUpdate = [channels[c] for c in globals.channels
+                if timestamp - channels[c].ffzCache >= cooldown and
+                channels[c].streamingSince is not None]
+    if not toUpdate:
+        toUpdate = [channels[c] for c in globals.channels
+                    if timestamp - channels[c].ffzCache >= cooldown and
+                    channels[c].streamingSince is None]
+    if toUpdate:
+        random.choice(toUpdate).updateFfzEmotes()
 
 def refreshBetterTwitchTvEmotes(timestamp):
-    for chan in globals.channels:
-        since = timestamp - globals.channels[chan].bttvCache
-        if since >= datetime.timedelta(hours=1):
-            globals.channels[chan].updateBttvEmotes()
-            return
+    cooldown = datetime.timedelta(hours=1)
+    since = timestamp - globals.globalBttvEmotesCache
+    if since >= cooldown:
+        bttv.updateGlobalEmotes()
+    channels = copy.copy(globals.channels)
+    toUpdate = [channels[c] for c in globals.channels
+                if timestamp - channels[c].bttvCache >= cooldown and
+                channels[c].streamingSince is not None]
+    if not toUpdate:
+        toUpdate = [channels[c] for c in globals.channels
+                    if timestamp - channels[c].bttvCache >= cooldown and
+                    channels[c].streamingSince is None]
+    if toUpdate:
+        random.choice(toUpdate).updateBttvEmotes()
