58fa77e0dd6e98ac1af5907431e954ea5e05339c
MeGotsThis <megotsthis@gmail.com>
2016-05-25 08:14:42 -0700

    Added WhisperCommandArgs

--- source/params/argument.py
+++ source/params/argument.py
@@ -3,3 +3,7 @@ from collections import namedtuple
 ChatCommandArgs = namedtuple('ChatCommandArgs',
                              ['database', 'chat', 'tags', 'nick', 'message',
                               'permissions', 'timestamp',])
+
+WhisperCommandArgs = namedtuple('WhisperCommandArgs',
+                                ['database', 'nick', 'message', 'permissions',
+                                 'timestamp',])
