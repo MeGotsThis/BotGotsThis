b7590152b7c66fee464f6073607c0384cb05ba98
MeGotsThis <megotsthis@gmail.com>
2016-05-25 08:15:55 -0700

    Slight rename

--- source/public/library/managebot.py
+++ source/public/library/managebot.py
@@ -1,7 +1,7 @@
 ﻿from lists.manage import methods
 
-def botManageBot(db, send, nick, message):
-    params = db, send, nick, message
+def botManageBot(database, send, nick, message):
+    params = database, send, nick, message
     
     m = message.lower[1]
     if m in methods and methods[m]:
