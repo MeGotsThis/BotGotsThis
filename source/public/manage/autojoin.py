0e65d9d41d09f8ed4e815da3605389d60717b32e
MeGotsThis <megotsthis@gmail.com>
2016-05-25 17:30:04 -0700

    Refactored autojoin.py for string.format

--- source/public/manage/autojoin.py
+++ source/public/manage/autojoin.py
@@ -27,8 +27,8 @@ def manageAutoJoin(args):
         return False
     if args.message.lower[2] in ['add', 'insert', 'join']:
         if args.database.isChannelBannedReason(args.message.lower[3]):
-            args.send('Chat ' + args.message.lower[3]
-                      + ' is banned from joining')
+            args.send('Chat {channel} is banned from joining'.format(
+                channel=args.message.lower[3]))
             return True
         cluster = twitch.twitchChatServer(args.message.lower[3])
         params = args.message.lower[3], 0, cluster
@@ -45,37 +45,33 @@ def manageAutoJoin(args):
                 args.message.lower[3], priority, cluster)
         
         if result and not wasInChat:
-            args.send('Auto join for ' + args.message.lower[3] + ' is now '
-                      'enabled and joined ' + args.message.lower[3] + ' chat')
+            msg = ('Auto join for {channel} is now enabled and joined '
+                   '{channel} chat')
         elif result:
             if rejoin < 0:
-                msg = 'Auto join for ' + args.message.lower[3]
-                msg += ' is now enabled and moved to the correct server'
+                msg = ('Auto join for {channel} is now enabled and moved to '
+                       'the correct server')
             else:
-                msg = 'Auto join for ' + args.message.lower[3] + ' is now'
-                msg += ' enabled'
-            args.send(msg)
+                msg = 'Auto join for {channel} is now enabled'
         elif not wasInChat:
-            args.send('Auto join for ' + args.message.lower[3] + ' is already '
-                      'enabled but now joined ' + args.message.lower[3] +
-                      ' chat')
+            msg = ('Auto join for {channel} is already enabled but now joined '
+                   '{channel} chat')
         else:
             if rejoin < 0:
-                msg = 'Auto join for ' + args.message.lower[3]
-                msg += ' is already enabled and moved to the correct server'
+                msg = ('Auto join for {channel} is already enabled and moved '
+                       'to the correct server')
             else:
-                msg = 'Auto join for ' + args.message.lower[3]
-                msg += ' is already enabled and already in chat'
-            args.send(msg)
+                msg = ('Auto join for {channel} is already enabled and '
+                       'already in chat')
+        args.send(msg.format(channel=args.message.lower[3]))
         return True
     if args.message.lower[2] in ['del', 'delete', 'rem', 'remove', 'remove']:
         result = args.database.discardAutoJoin(args.message.lower[3])
         if result:
-            args.send('Auto join for ' + args.message.lower[3]
-                      + ' is now disabled')
+            msg = 'Auto join for {channel} is now disabled'
         else:
-            args.send('Auto join for ' + args.message.lower[3]
-                      + ' was never enabled')
+            msg = 'Auto join for {channel} was never enabled'
+        args.send(msg.format(channel=args.message.lower[3]))
         return True
     if args.message.lower[2] in ['pri', 'priority']:
         try:
@@ -85,10 +81,10 @@ def manageAutoJoin(args):
         result = args.database.setAutoJoinPriority(
             args.message.lower[3], priority)
         if result:
-            args.send('Auto join for ' + args.message.lower[3]
-                       + ' is set to priority ' + str(priority))
+            msg = 'Auto join for {channel} is set to priority {priority}'
         else:
-            args.send('Auto join for ' + args.message.lower[3]
-                      + ' was never enabled')
+            msg = 'Auto join for {channel} was never enabled'
+        args.send(msg.format(channel=args.message.lower[3],
+                             priority=priority))
         return True
     return False
