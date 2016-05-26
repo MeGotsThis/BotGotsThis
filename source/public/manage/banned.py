0185208846c960f5c9b743c8d31a5b9fc4c09575
MeGotsThis <megotsthis@gmail.com>
2016-05-25 17:43:01 -0700

    Refactored banned.py

--- source/public/manage/banned.py
+++ source/public/manage/banned.py
@@ -1,52 +1,54 @@
 ﻿from bot import utils
 
-def manageBanned(database, send, nick, message):
-    if len(message) < 3:
+needReason = ['add', 'insert', 'del', 'delete', 'rem', 'remove', 'remove']
+
+def manageBanned(args):
+    if len(args.message) < 3:
         return False
-    if message.lower[2] in ['list']:
-        bannedChannels = database.listBannedChannels()
+    if args.message.lower[2] in ['list']:
+        bannedChannels = args.database.listBannedChannels()
         if bannedChannels:
             msg = 'Banned Channels: ' + ', '.join(bannedChannels)
-            send(msg)
         else:
-            send('There are no banned channels')
+            msg = 'There are no banned channels'
+        args.send(msg)
         return True
     
-    if len(message) < 5:
-        if message.lower[2] in ['add', 'insert', 'del', 'delete',
-                                'rem', 'remove', 'remove']:
-            send(nick + ' -> Reason needs to be specified')
+    if len(args.message) < 5:
+        if args.message.lower[2] in needReason:
+            args.send(args.nick + ' -> Reason needs to be specified')
         return False
-    channel = message.lower[3]
-    if message.lower[2] in ['add', 'insert']:
-        isBannedOrReason = database.isChannelBannedReason(channel)
+    channel = args.message.lower[3]
+    if args.message.lower[2] in ['add', 'insert']:
+        isBannedOrReason = args.database.isChannelBannedReason(channel)
         if isBannedOrReason:
-            send(channel + ' is already banned for: ' +
-                        isBannedOrReason)
+            args.send('{channel} is already banned for: {reason}'.format(
+                channel=channel, reason = isBannedOrReason))
             return False
-        params = channel, message[4:], nick
-        result = database.addBannedChannel(*params)
+        params = channel, args.message[4:], args.nick
+        result = args.database.addBannedChannel(*params)
         if result:
-            database.discardAutoJoin(channel)
+            args.database.discardAutoJoin(channel)
             utils.partChannel(channel)
             
         if result:
-            send('Chat ' + channel + ' is now banned')
+            msg = 'Chat {channel} is now banned'
         else:
-            send('Chat ' + channel + ' could not be banned. '
-                        'Error has occured.')
+            msg = 'Chat {channel} could not be banned. Error has occured.'
+        args.send(msg.format(channel=channel))
         return True
-    if message.lower[2] in ['del', 'delete', 'rem', 'remove', 'remove']:
-        isBannedOrReason = database.isChannelBannedReason(channel)
+    if args.message.lower[2] in ['del', 'delete', 'rem', 'remove', 'remove']:
+        isBannedOrReason = args.database.isChannelBannedReason(channel)
         if not isBannedOrReason:
-            send(channel + ' is not banned')
+            args.send('{channel} is not banned'.format(channel=channel))
             return False
-        params = channel, message[4:], nick
-        result = database.removeBannedChannel(*params)
+        params = channel, args.message[4:], args.nick
+        result = args.database.removeBannedChannel(*params)
             
         if result:
-            send(channel + ' is now unbanned')
+            msg =  'Chat {channel} is now unbanned'
         else:
-            send(channel + ' could not be unbanned. Error has occured.')
+            msg = 'Chat {channel} could not be unbanned. Error has occured.'
+        args.send(msg.format(channel=channel))
         return True
     return False
