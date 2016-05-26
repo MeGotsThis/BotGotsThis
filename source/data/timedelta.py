6f480fd54164306de27fa1b8277013a6bfaf2cf6
MeGotsThis <megotsthis@gmail.com>
2016-05-25 17:19:05 -0700

    string.format refactoring in timedelta.py

--- source/data/timedelta.py
+++ source/data/timedelta.py
@@ -3,20 +3,20 @@
     if timeDelta.days == 1:
         formatted.append('1 day')
     elif timeDelta.days > 1:
-        formatted.append(str(timeDelta.days) + ' days')
+        formatted.append('{days} days'.format(days=timeDelta.days))
     hours = timeDelta.seconds // 3600
     if hours == 1:
         formatted.append('1 hour')
     elif hours > 1:
-        formatted.append(str(hours) + ' hours')
+        formatted.append('{hours} hours'.format(hours=hours))
     minutes = timeDelta.seconds // 60 % 60
     if minutes == 1:
         formatted.append('1 minute')
     elif minutes > 1:
-        formatted.append(str(minutes) + ' minutes')
+        formatted.append('{minutes} minutes'.format(minutes=minutes))
     seconds = timeDelta.seconds % 60
     if seconds == 1:
         formatted.append('1 second')
     elif seconds > 1:
-        formatted.append(str(seconds) + ' seconds')
+        formatted.append('{seconds} seconds'.format(seconds=seconds))
     return ', '.join(formatted)
