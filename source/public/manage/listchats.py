204584b55c761afc1666b5c87dc5834dff979afa
MeGotsThis <megotsthis@gmail.com>
2016-05-25 17:42:50 -0700

    Refactored listchats.py to use ManageBotArgs

--- source/public/manage/listchats.py
+++ source/public/manage/listchats.py
@@ -1,9 +1,9 @@
 ﻿from bot import config, globals
 from collections import deque
 
-def manageListChats(database, send, nick, message):
+def manageListChats(args):
     prepend = 'Twitch Chats: '
-    limit = config.messageLimit - len(prepend)
+    limit = config.args.messageLimit - len(prepend)
     channels = deque(sorted(c for c in globals.channels.keys()))
     chanList = []
     length = 0
@@ -15,13 +15,13 @@ def manageListChats(database, send, nick, message):
         length += len(chan)
         if length >= limit:
             if len(chanList) > 1:
-                send(prepend + ', '.join(chanList[:-1]))
+                args.send(prepend + ', '.join(chanList[:-1]))
                 del chanList[:-1]
                 length = len(chan)
             else:
-                send(prepend + ', '.join(chanList))
+                args.send(prepend + ', '.join(chanList))
                 chanList.clear()
                 length = 0
     if chanList:
-        send(prepend + ', '.join(chanList))
+        args.send(prepend + ', '.join(chanList))
     return True
