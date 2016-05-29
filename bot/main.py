f1189ca5779918f1ef32b5e649170bc9268ee25a
MeGotsThis <megotsthis@gmail.com>
2016-05-28 22:07:12 -0700

    Created a main method

--- bot/main.py
+++ bot/main.py
@@ -16,48 +16,50 @@ import threading
 import time
 import traceback
 
-print('{time} Starting'.format(time=datetime.datetime.utcnow()))
-globals.messaging = MessageQueue(name='Message Queue')
+def main(argv):
+    print('{time} Starting'.format(time=datetime.datetime.utcnow()))
+    globals.messaging = MessageQueue(name='Message Queue')
 
-globals.clusters['aws'] = SocketThread(config.awsServer, config.awsPort,
-                                       name='AWS Chat')
+    globals.clusters['aws'] = SocketThread(config.awsServer, config.awsPort,
+                                           name='AWS Chat')
 
-globals.join = JoinThread(name='Join Thread')
-globals.groupChannel = Channel('jtv', globals.clusters['aws'],
-                               float('-inf'))
+    globals.join = JoinThread(name='Join Thread')
+    globals.groupChannel = Channel('jtv', globals.clusters['aws'],
+                                   float('-inf'))
 
-globals.background = BackgroundTasker(name='Background Tasker')
+    globals.background = BackgroundTasker(name='Background Tasker')
 
-# Start the Threads
-for st in globals.clusters.values():
-    if st:
-        st.start()
-globals.messaging.start()
-globals.background.start()
-globals.join.start()
+    # Start the Threads
+    for st in globals.clusters.values():
+        if st:
+            st.start()
+    globals.messaging.start()
+    globals.background.start()
+    globals.join.start()
 
-_modulesList = [
-    pkgutil.walk_packages(path=publicAuto.__path__,
-                          prefix=publicAuto.__name__+'.'),
-    pkgutil.walk_packages(path=privateAuto.__path__,
-                          prefix=privateAuto.__name__+'.')
-    ]
-for _modules in _modulesList:
-    for importer, modname, ispkg in _modules:
-          importlib.import_module(modname)
+    _modulesList = [
+        pkgutil.walk_packages(path=publicAuto.__path__,
+                              prefix=publicAuto.__name__+'.'),
+        pkgutil.walk_packages(path=privateAuto.__path__,
+                              prefix=privateAuto.__name__+'.')
+        ]
+    for _modules in _modulesList:
+        for importer, modname, ispkg in _modules:
+              importlib.import_module(modname)
 
-try:
-    utils.joinChannel(config.botnick, float('-inf'), 'aws')
-    if config.owner:
-        utils.joinChannel(config.owner, float('-inf'), 'aws')
-    with getDatabase() as db:
-        for channel in db.getAutoJoinsChats():
-            utils.joinChannel(channel.broadcaster, channel.priority,
-                              channel.cluster)
+    try:
+        utils.joinChannel(config.botnick, float('-inf'), 'aws')
+        if config.owner:
+            utils.joinChannel(config.owner, float('-inf'), 'aws')
+        with getDatabase() as db:
+            for channel in db.getAutoJoinsChats():
+                utils.joinChannel(channel.broadcaster, channel.priority,
+                                  channel.cluster)
     
-    globals.messaging.join()
-except:
-    utils.logException()
-    raise
-finally:
-    print('{time} Ended'.format(time=datetime.datetime.utcnow()))
+        globals.messaging.join()
+        return 0
+    except:
+        utils.logException()
+        raise
+    finally:
+        print('{time} Ended'.format(time=datetime.datetime.utcnow()))
--- main.py
+++ main.py
@@ -1,1 +1,3 @@
 ﻿import bot.main
+import sys
+sys.exit(bot.main.main(sys.argv))
