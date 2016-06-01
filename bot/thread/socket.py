b26f877b78051354144200b6a64be96f4ffd1b96
MeGotsThis <megotsthis@gmail.com>
2016-05-31 21:49:27 -0700

    Moved the logic in SocketThread

--- bot/thread/socket.py
+++ bot/thread/socket.py
@@ -18,26 +18,31 @@ class SocketsThread(threading.Thread):
         print('{time} Starting {name}'.format(
             time=datetime.utcnow(), name=self.__class__.__name__))
         
-        isActive = lambda s: s.socket
         while globals.running:
-            for socketConnection in filterfalse(isActive,
-                                                self._socketConnections):
-                socketConnection.connect()
-            connections = list(filter(isActive, self._socketConnections))
-            if connections:
-                read, write, exceptional = select.select(
-                    connections, connections, connections, 0.01)
-                for socketConnection in read:
-                    socketConnection.read()
-                for socketConnection in write:
-                    socketConnection.flushWrite()
-            for socketConnection in filter(isActive, self._socketConnections):
-                socketConnection.sendPing()
+            self.process()
+        self.terminate()
         
-        for socketConnection in self._socketConnections:
-            socketConnection.disconnect()
         print('{time} Ending {name}'.format(
             time=datetime.utcnow(), name=self.__class__.__name__))
     
     def register(self, socketConn):
         self._socketConnections.append(socketConn)
+    
+    def process(self):
+        isActive = lambda s: s.socket
+        for socketConnection in filterfalse(isActive, self._socketConnections):
+            socketConnection.connect()
+        connections = list(filter(isActive, self._socketConnections))
+        if connections:
+            read, write, exceptional = select.select(
+                connections, connections, connections, 0.01)
+            for socketConnection in read:
+                socketConnection.read()
+            for socketConnection in write:
+                socketConnection.flushWrite()
+        for socketConnection in filter(isActive, self._socketConnections):
+            socketConnection.sendPing()
+    
+    def terminate(self):
+        for socketConnection in self._socketConnections:
+            socketConnection.disconnect()
