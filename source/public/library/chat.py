06a802c4fc4677c13c9a5320a603ad2134a49aea
MeGotsThis <megotsthis@gmail.com>
2016-06-02 12:29:44 -0700

    Added some decorators for chat commands

---
+++ source/public/library/chat.py
@@ -0,0 +1,19 @@
+from functools import wraps
+
+def permission(permission):
+    def decorator(func):
+        @wraps(func)
+        def chatCommand(args):
+            if not args.permissions[permission]:
+                return False
+            return func(args)
+        return chatCommand
+    return decorator
+
+def ownerChannel(func):
+    @wraps(func)
+    def chatCommand(args):
+        if not args.permissions.inOwnerChannel:
+            return False
+        return func(args)
+    return chatCommand
