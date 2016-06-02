b42b2b9999ccd9889163a289ec1a6372f35393af
MeGotsThis <megotsthis@gmail.com>
2016-06-02 01:36:52 -0700

    Made DatabaseBase into an abstract class

--- source/database/databasebase.py
+++ source/database/databasebase.py
@@ -1,6 +1,7 @@
 from bot.data.return_ import AutoJoinChannel
+from abc import ABCMeta, abstractmethod
 
-class DatabaseBase:
+class DatabaseBase(metaclass=ABCMeta):
     __slots__ = ('_engine', '_connection')
     
     def __init__(self):
@@ -23,55 +24,71 @@ class DatabaseBase:
         if self.connection is not None:
             self.connection.close()
     
+    @abstractmethod
     def getAutoJoinsChats(self):
         yield from []
     
+    @abstractmethod
     def getAutoJoinsPriority(self, broadcaster):
         return float('inf')
     
+    @abstractmethod
     def saveAutoJoin(self, broadcaster, priority=0, cluster='aws'):
         return False
     
+    @abstractmethod
     def discardAutoJoin(self, broadcaster):
         return False
     
+    @abstractmethod
     def setAutoJoinPriority(self, broadcaster, priority):
         return False
     
+    @abstractmethod
     def setAutoJoinServer(self, broadcaster, cluster='aws'):
         return False
     
+    @abstractmethod
     def getOAuthToken(self, broadcaster):
         return None
     
+    @abstractmethod
     def saveBroadcasterToken(self, broadcaster, token):
         pass
     
+    @abstractmethod
     def getChatCommands(self, broadcaster, command):
         return {broadcaster: {}, '#global': {}}
     
+    @abstractmethod
     def getFullGameTitle(self, abbreviation):
         return None
     
+    @abstractmethod
     def insertCustomCommand(self, broadcaster, permission, command,
                             fullMessage, user):
         return False
     
+    @abstractmethod
     def updateCustomCommand(self, broadcaster, permission, command,
                             fullMessage, user):
         return False
     
+    @abstractmethod
     def replaceCustomCommand(self, broadcaster, permission, command,
                              fullMessage, user):
         return False
     
+    @abstractmethod
     def appendCustomCommand(self, broadcaster, permission, command,
                              message, user):
         return False
     
+    @abstractmethod
     def deleteCustomCommand(self, broadcaster, permission, command, user):
         return True
     
+    @abstractmethod
     def getCustomCommandProperty(self, broadcaster, permission, command,
                                  property=None):
         if property is None:
@@ -81,41 +98,53 @@ class DatabaseBase:
         else:
             return None
     
+    @abstractmethod
     def processCustomCommandProperty(self, broadcaster, permission, command,
                                      property, value):
         return False
     
+    @abstractmethod
     def hasFeature(self, broadcaster, feature):
         return False
     
+    @abstractmethod
     def addFeature(self, broadcaster, feature):
         return False
     
+    @abstractmethod
     def removeFeature(self, broadcaster, feature):
         return True
     
+    @abstractmethod
     def listBannedChannels(self):
         yield from []
     
+    @abstractmethod
     def isChannelBannedReason(self, broadcaster):
         return None
     
+    @abstractmethod
     def addBannedChannel(self, broadcaster, reason, nick):
         return True
     
+    @abstractmethod
     def removeBannedChannel(self, broadcaster, reason, nick):
         return True
     
+    @abstractmethod
     def recordTimeout(self, broadcaster, user, fromUser, module, level, length,
                       message, reason):
         return False
     
+    @abstractmethod
     def getChatProperty(self, broadcaster, property, default=None, parse=None):
         return default
     
+    @abstractmethod
     def getChatProperties(self, broadcaster, properties=[], default=None,
                           parse=None):
         return {}
     
+    @abstractmethod
     def setChatProperty(self, broadcaster, property, value=None):
         return False
