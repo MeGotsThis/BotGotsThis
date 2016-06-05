26d5b930ffed52e15e95476432b9c1a929bb007f
MeGotsThis <megotsthis@gmail.com>
2016-06-04 20:56:03 -0700

    Refactored listchats

---
+++ source/public/library/message.py
@@ -0,0 +1,27 @@
+from bot import config
+from collections import deque
+
+def messagesFromItems(items, prepend=None):
+    limit = config.messageLimit - len(prepend)
+    queue = deque(items)
+    itemsMsg = deque()
+    length = 0
+    while queue:
+        item = queue.popleft()
+        itemsMsg.append(item)
+        if length:
+            length += 2
+        length += len(item)
+        if length >= limit:
+            if len(itemsMsg) > 1:
+                itemsMsg.pop()
+                yield prepend + ', '.join(itemsMsg)
+                itemsMsg.clear()
+                itemsMsg.append(item)
+                length = len(item)
+            else:
+                yield prepend + ', '.join(itemsMsg)
+                itemsMsg.clear()
+                length = 0
+    if itemsMsg:
+        yield prepend + ', '.join(itemsMsg)
--- source/public/manage/listchats.py
+++ source/public/manage/listchats.py
@@ -1,27 +1,11 @@
-﻿from bot import config, globals
+﻿from  ..library.message import messagesFromItems
+from bot import config, globals
 from collections import deque
 
 def manageListChats(args):
-    prepend = 'Twitch Chats: '
-    limit = config.args.messageLimit - len(prepend)
-    channels = deque(sorted(c for c in globals.channels.keys()))
-    chanList = []
-    length = 0
-    while channels:
-        chan = channels.popleft()
-        chanList.append(chan)
-        if length:
-            length += 2
-        length += len(chan)
-        if length >= limit:
-            if len(chanList) > 1:
-                args.send(prepend + ', '.join(chanList[:-1]))
-                del chanList[:-1]
-                length = len(chan)
-            else:
-                args.send(prepend + ', '.join(chanList))
-                chanList.clear()
-                length = 0
-    if chanList:
-        args.send(prepend + ', '.join(chanList))
+    if globals.channels:
+        channels = sorted(c for c in globals.channels.keys())
+        args.send(messagesFromItems(channels, 'Twitch Chats: '))
+    else:
+        args.send('I am somehow not in any channels, please help me here')
     return True
