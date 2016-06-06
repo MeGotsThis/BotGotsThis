9be08aa9999f72d3dc190cece0f241bc0bb0fd8f
MeGotsThis <megotsthis@gmail.com>
2016-06-05 21:31:19 -0700

    Re-factored url.py

--- source/public/custom/url.py
+++ source/public/custom/url.py
@@ -1,4 +1,6 @@
 ﻿from bot import config
+from contextlib import suppress
+import urllib.error
 import urllib.request
 
 def fieldUrl(args):
@@ -8,16 +10,14 @@ def fieldUrl(args):
         url = url.replace('{nick}', args.nick)
         url = url.replace('{broadcaster}', args.channel)
         url = url.replace('{streamer}', args.channel)
-        try:
-            urlopen = urllib.request.urlopen
-            req = urlopen(url, timeout=config.customMessageUrlTimeout)
+        with suppress(urllib.error.URLError):
+            req = urllib.request.urlopen(
+                url, timeout=config.customMessageUrlTimeout)
             if int(req.status) == 200:
                 data = req.read().decode('utf-8')
                 data = data.replace('\r\n', ' ')
                 data = data.replace('\n', ' ')
                 data = data.replace('\r', ' ')
                 return args.prefix + data + args.suffix
-        except:
-            pass
         return args.default
     return None
