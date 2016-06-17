a7e0bffd77156aff06530298cb540de795e3f67a
MeGotsThis <megotsthis@gmail.com>
2016-06-16 17:30:04 -0700

    Changed the unit tests in TestsIrcTagsKey to many smaller tests

--- unittests/twitchmessage/test_irctagskey.py
+++ unittests/twitchmessage/test_irctagskey.py
@@ -1,117 +1,221 @@
 import unittest
-from collections.abc import Hashable
+from collections.abc import Hashable as HashableAbc
+from typing import Hashable
 from bot.twitchmessage import IrcMessageTagsKey
 from bot.twitchmessage._irctags import ParsedKeyVendor
 
 
 class TestsIrcTagsKey(unittest.TestCase):
-    def test_constructor(self):
+    def test_empty_constructor(self):
         key = IrcMessageTagsKey()
         self.assertEqual(key.vendor, None)
         self.assertEqual(key.key, '')
 
-        key = IrcMessageTagsKey('Kappa')
+    def test_constructor_key(self):
+        key = IrcMessageTagsKey(key='Kappa')
         self.assertEqual(key.vendor, None)
         self.assertEqual(key.key, 'Kappa')
 
-        self.assertRaises(TypeError, IrcMessageTagsKey, None)
-        self.assertRaises(TypeError, IrcMessageTagsKey, b'Kappa')
-        self.assertRaises(TypeError, IrcMessageTagsKey, 123)
-        self.assertRaises(TypeError, IrcMessageTagsKey, b'Kappa', None)
-        self.assertRaises(TypeError, IrcMessageTagsKey, b'Kappa', 'Keepo')
-        self.assertRaises(TypeError, IrcMessageTagsKey, 'Kappa', b'Keepo')
+    def test_constructor_key_None(self):
+        self.assertRaises(TypeError, IrcMessageTagsKey, key=None)
 
+    def test_constructor_key_bytes(self):
+        self.assertRaises(TypeError, IrcMessageTagsKey, key=b'Kappa')
+
+    def test_constructor_key_int(self):
+        self.assertRaises(TypeError, IrcMessageTagsKey, key=123)
+
+    def test_constructor_key_bytes_vendor_None(self):
+        self.assertRaises(TypeError, IrcMessageTagsKey, vendor=None, key=b'Kappa')
+
+    def test_constructor_key_bytes_vendor(self):
+        self.assertRaises(TypeError, IrcMessageTagsKey, vendor='Keepo', key=b'Kappa')
+
+    def test_constructor_key_vendor_bytes(self):
+        self.assertRaises(TypeError, IrcMessageTagsKey, vendor=b'Keepo', key='Kappa')
+
+    def test_constructor_key_vendor_None(self):
         key = IrcMessageTagsKey('Kappa', None)
         self.assertEqual(key.vendor, None)
         self.assertEqual(key.key, 'Kappa')
 
+    def test_constructor_key_vendor(self):
         key = IrcMessageTagsKey('Kappa', 'Keepo')
         self.assertEqual(key.vendor, 'Keepo')
         self.assertEqual(key.key, 'Kappa')
 
-    def test_magic_str(self):
+    def test_constructor_vendor(self):
+        key = IrcMessageTagsKey(vendor='Kappa')
+        self.assertEqual(key.vendor, 'Kappa')
+        self.assertEqual(key.key, '')
+
+    def test_constructor_keywords(self):
+        key = IrcMessageTagsKey(vendor='Keepo', key='Kappa')
+        self.assertEqual(key.vendor, 'Keepo')
+        self.assertEqual(key.key, 'Kappa')
+
+    def test_magic_str_empty(self):
         key = IrcMessageTagsKey()
         self.assertEqual(str(key), '')
 
-        key = IrcMessageTagsKey('Kappa')
+    def test_magic_str_key(self):
+        key = IrcMessageTagsKey(key='Kappa')
         self.assertEqual(str(key), 'Kappa')
 
-        key = IrcMessageTagsKey('Kappa', 'Keepo')
+    def test_magic_str_key_vendor(self):
+        key = IrcMessageTagsKey(vendor='Keepo', key='Kappa')
         self.assertEqual(str(key), 'Keepo/Kappa')
 
-        key = IrcMessageTagsKey('', 'Keepo')
+    def test_magic_str_vendor(self):
+        key = IrcMessageTagsKey(vendor='Keepo')
         self.assertEqual(str(key), 'Keepo/')
 
-    def test_from_str(self):
-        self.assertRaises(TypeError, IrcMessageTagsKey.fromKeyVendor, b'a')
+    def test_from_str_bytes(self):
+        self.assertRaises(TypeError, IrcMessageTagsKey.fromKeyVendor, b'')
+
+    def test_from_str_integer(self):
         self.assertRaises(TypeError, IrcMessageTagsKey.fromKeyVendor, 1)
 
+    def test_from_str_empty(self):
         key = IrcMessageTagsKey.fromKeyVendor('')
         self.assertEqual(key.vendor, None)
         self.assertEqual(key.key, '')
 
+    def test_from_str_key(self):
         key = IrcMessageTagsKey.fromKeyVendor('DansGame')
         self.assertEqual(key.vendor, None)
         self.assertEqual(key.key, 'DansGame')
 
+    def test_from_str_key_numbers(self):
         key = IrcMessageTagsKey.fromKeyVendor('123')
         self.assertEqual(key.vendor, None)
         self.assertEqual(key.key, '123')
 
-        key = IrcMessageTagsKey.fromKeyVendor('DansGame/')
-        self.assertEqual(key.vendor, 'DansGame')
-        self.assertEqual(key.key, '')
+    def test_equals_key(self):
+        self.assertEqual(IrcMessageTagsKey.fromKeyVendor('abc'),
+                         IrcMessageTagsKey(key='abc'))
 
-        key = IrcMessageTagsKey.fromKeyVendor('DansGame/SwiftRage')
-        self.assertEqual(key.vendor, 'DansGame')
-        self.assertEqual(key.key, 'SwiftRage')
-
-        key = IrcMessageTagsKey.fromKeyVendor('DansGame.com/SwiftRage')
-        self.assertEqual(key.vendor, 'DansGame.com')
-        self.assertEqual(key.key, 'SwiftRage')
-
-        self.assertRaises(ValueError, IrcMessageTagsKey.fromKeyVendor, '!')
-        self.assertRaises(ValueError, IrcMessageTagsKey.fromKeyVendor, '@')
-        self.assertRaises(ValueError, IrcMessageTagsKey.fromKeyVendor, '#')
-        self.assertRaises(ValueError, IrcMessageTagsKey.fromKeyVendor, '/')
-        self.assertRaises(ValueError, IrcMessageTagsKey.fromKeyVendor, '1/')
-        self.assertRaises(ValueError, IrcMessageTagsKey.fromKeyVendor, 'abc.cd./abc')
-        self.assertRaises(ValueError, IrcMessageTagsKey.fromKeyVendor, 'abc.d')
-        self.assertRaises(ValueError, IrcMessageTagsKey.fromKeyVendor, '/abc')
-        self.assertRaises(ValueError, IrcMessageTagsKey.fromKeyVendor, ' ')
-        self.assertRaises(ValueError, IrcMessageTagsKey.fromKeyVendor, 'a ')
-        self.assertRaises(ValueError, IrcMessageTagsKey.fromKeyVendor, ' a')
-        self.assertRaises(ValueError, IrcMessageTagsKey.fromKeyVendor, ' a ')
-
-    def test_equals(self):
+    def test_equals_key_vendor(self):
         self.assertEqual(IrcMessageTagsKey.fromKeyVendor('OpieOP.SoBayed/PJSalt'),
-                         IrcMessageTagsKey('PJSalt', 'OpieOP.SoBayed'))
+                         IrcMessageTagsKey(vendor='OpieOP.SoBayed', key='PJSalt'))
+
+    def test_not_equals_key(self):
         self.assertNotEqual(IrcMessageTagsKey.fromKeyVendor('abc'),
-                            IrcMessageTagsKey('123'))
+                            IrcMessageTagsKey(key='123'))
+
+    def test_not_equals_key_vendor_by_key(self):
+        self.assertNotEqual(IrcMessageTagsKey.fromKeyVendor('OpieOP.SoBayed/PJSalt'),
+                            IrcMessageTagsKey(vendor='OpieOP.SoBayed', key='DansGame'))
+
+    def test_not_equals_key_vendor_by_vendor(self):
+        self.assertNotEqual(IrcMessageTagsKey.fromKeyVendor('OpieOP.SoBayed/PJSalt'),
+                            IrcMessageTagsKey(vendor='SoBayed.OpieOP', key='PJSalt'))
+
+    def test_not_equals_key_vendor(self):
+        self.assertNotEqual(IrcMessageTagsKey.fromKeyVendor('OpieOP.SoBayed/PJSalt'),
+                            IrcMessageTagsKey(vendor='SoBayed.PJSalt', key='OpieOP'))
 
     def test_hash(self):
         key = IrcMessageTagsKey()
         self.assertIsInstance(hash(key), int)
+        self.assertIsInstance(key, HashableAbc)
         self.assertIsInstance(key, Hashable)
+
+    def test_parse_symbol_exclamation(self):
+        self.assertRaises(ValueError, IrcMessageTagsKey.parse, '!')
+
+    def test_parse_symbol_at(self):
+        self.assertRaises(ValueError, IrcMessageTagsKey.parse, '@')
+
+    def test_parse_symbol_number_sign(self):
+        self.assertRaises(ValueError, IrcMessageTagsKey.parse, '#')
+
+    def test_parse_forward_slash(self):
+        self.assertRaises(ValueError, IrcMessageTagsKey.parse, '/')
+
+    def test_parse_number_vendor(self):
+        self.assertRaises(ValueError, IrcMessageTagsKey.parse, '1/')
+
+    def test_parse_empty_vendor(self):
+        self.assertRaises(ValueError, IrcMessageTagsKey.parse, '/abc')
+
+    def test_parse_space(self):
+        self.assertRaises(ValueError, IrcMessageTagsKey.parse, ' ')
+
+    def test_parse_trailing_space(self):
+        self.assertRaises(ValueError, IrcMessageTagsKey.parse, 'a ')
+
+    def test_parse_leading_space(self):
+        self.assertRaises(ValueError, IrcMessageTagsKey.parse, ' a')
+
+    def test_parse_covering_space(self):
+        self.assertRaises(ValueError, IrcMessageTagsKey.parse, ' a ')
+
+    def test_parse_middle_space(self):
+        self.assertRaises(ValueError, IrcMessageTagsKey.parse, 'a b')
+
+    def test_parse_period(self):
+        self.assertRaises(ValueError, IrcMessageTagsKey.parse, '.')
+
+    def test_parse_key_leading_period(self):
+        self.assertRaises(ValueError, IrcMessageTagsKey.parse, '.a')
+
+    def test_parse_key_trailing_period(self):
+        self.assertRaises(ValueError, IrcMessageTagsKey.parse, 'a.')
+
+    def test_parse_key_covering_period(self):
+        self.assertRaises(ValueError, IrcMessageTagsKey.parse, '.a.')
+
+    def test_parse_key_containing_period(self):
+        self.assertRaises(ValueError, IrcMessageTagsKey.parse, 'a.b')
+
+    def test_parse_vendor_period(self):
+        self.assertRaises(ValueError, IrcMessageTagsKey.parse, './')
+
+    def test_parse_vendor_leading_period(self):
+        self.assertRaises(ValueError, IrcMessageTagsKey.parse, '.a/')
+
+    def test_parse_vendor_trailing_period(self):
+        self.assertRaises(ValueError, IrcMessageTagsKey.parse, 'a./')
+
+    def test_parse_vendor_surrounding_period(self):
+        self.assertRaises(ValueError, IrcMessageTagsKey.parse, '.a./')
+
+    def test_parse_vendor_multiple_leading_period(self):
+        self.assertRaises(ValueError, IrcMessageTagsKey.parse, '.a.a/')
+
+    def test_parse_vendor_multiple_trailing_period(self):
+        self.assertRaises(ValueError, IrcMessageTagsKey.parse, 'a.a./')
+
+    def test_parse_vendor_multiple_convering_period(self):
+        self.assertRaises(ValueError, IrcMessageTagsKey.parse, '.a.a./')
 
     def test_parse(self):
         self.assertEquals(IrcMessageTagsKey.parse(''), ParsedKeyVendor('', None))
+
+    def test_parse_key(self):
         self.assertEquals(IrcMessageTagsKey.parse('Kreygasm'),
                           ParsedKeyVendor('Kreygasm', None))
-        self.assertEquals(IrcMessageTagsKey.parse('PogChamp/Kreygasm'),
-                          ParsedKeyVendor('Kreygasm', 'PogChamp'))
+
+    def test_parse_key_vendor(self):
         self.assertEquals(IrcMessageTagsKey.parse('PogChamp/Kreygasm'),
                           ParsedKeyVendor('Kreygasm', 'PogChamp'))
 
-        self.assertRaises(ValueError, IrcMessageTagsKey.parse, '!')
-        self.assertRaises(ValueError, IrcMessageTagsKey.parse, '@')
-        self.assertRaises(ValueError, IrcMessageTagsKey.parse, '#')
-        self.assertRaises(ValueError, IrcMessageTagsKey.parse, '/')
-        self.assertRaises(ValueError, IrcMessageTagsKey.parse, '/a')
-        self.assertRaises(ValueError, IrcMessageTagsKey.parse, ' ')
-        self.assertRaises(ValueError, IrcMessageTagsKey.parse, 'a ')
-        self.assertRaises(ValueError, IrcMessageTagsKey.parse, ' a')
-        self.assertRaises(ValueError, IrcMessageTagsKey.parse, ' a ')
-        self.assertRaises(ValueError, IrcMessageTagsKey.parse, '.')
-        self.assertRaises(ValueError, IrcMessageTagsKey.parse, 'a.')
-        self.assertRaises(ValueError, IrcMessageTagsKey.parse, '.a')
+    def test_parse_key_venor_domain(self):
+        self.assertEquals(IrcMessageTagsKey.parse('PogChamp/Kreygasm'),
+                          ParsedKeyVendor('Kreygasm', 'PogChamp'))
+
+    def test_from_str_vendor(self):
+        key = IrcMessageTagsKey.fromKeyVendor('DansGame/')
+        self.assertEqual(key.vendor, 'DansGame')
+        self.assertEqual(key.key, '')
+
+    def test_from_str_key_vendor(self):
+        key = IrcMessageTagsKey.fromKeyVendor('DansGame/SwiftRage')
+        self.assertEqual(key.vendor, 'DansGame')
+        self.assertEqual(key.key, 'SwiftRage')
+
+    def test_from_str_key_vendor_domain(self):
+        key = IrcMessageTagsKey.fromKeyVendor('DansGame.com/SwiftRage')
+        self.assertEqual(key.vendor, 'DansGame.com')
+        self.assertEqual(key.key, 'SwiftRage')
