06b97cb81463c1c4748a589383411efb82cce375
MeGotsThis <megotsthis@gmail.com>
2016-06-28 10:10:07 -0700

    Fixed reading emotes with 404 error. This is useful when the API goes away or the broadcaster is not registered with the respective sites

--- source/api/bttv.py
+++ source/api/bttv.py
@@ -1,36 +1,47 @@
 ﻿import json
-from contextlib import suppress
 from http.client import HTTPResponse
 from typing import Dict, Optional
-from urllib.error import URLError
+from urllib.error import HTTPError, URLError
 from urllib.request import urlopen
 
 
 def getGlobalEmotes() -> Optional[Dict[str, str]]:
     url = 'https://api.betterttv.net/2/emotes'
-    with suppress(URLError), urlopen(url) as response:
-        if not isinstance(response, HTTPResponse):
-            raise TypeError()
-        if response.status == 200:
-            responseData = response.read()  # type: bytes
-            bttvData = json.loads(responseData.decode())  # type: Dict[str, List[Dict[str, str]]]
-            emotes = {}  # type: Dict[str, str]
-            for emote in bttvData['emotes']:  # --type: Dict[str, str]
-                emotes[emote['id']] = emote['code']
-            return emotes
+    try:
+        with urlopen(url) as response:
+            if not isinstance(response, HTTPResponse):
+                raise TypeError()
+            if response.status == 200:
+                responseData = response.read()  # type: bytes
+                bttvData = json.loads(responseData.decode())  # type: dict
+                emotes = {}  # type: Dict[str, str]
+                for emote in bttvData['emotes']:  # --type: Dict[str, str]
+                    emotes[emote['id']] = emote['code']
+                return emotes
+    except HTTPError as e:
+        if e.code == 404:
+            return {}
+    except URLError:
+        pass
     return None
 
 
 def getBroadcasterEmotes(broadcaster: str) -> Optional[Dict[str, str]]:
     url = 'https://api.betterttv.net/2/channels/' + broadcaster
-    with suppress(URLError), urlopen(url) as response:
-        if not isinstance(response, HTTPResponse):
-            raise TypeError()
-        if response.status == 200:
-            responseData = response.read()  # type: bytes
-            bttvData = json.loads(responseData.decode())  # type: Dict[str, List[Dict[str, str]]]
-            emotes = {}  # type: Dict[str, str]
-            for emote in bttvData['emotes']:  # --type: Dict[str, str]
-                emotes[emote['id']] = emote['code']
-            return emotes
+    try:
+        with urlopen(url) as response:
+            if not isinstance(response, HTTPResponse):
+                raise TypeError()
+            if response.status == 200:
+                responseData = response.read()  # type: bytes
+                bttvData = json.loads(responseData.decode())  # type: dict
+                emotes = {}  # type: Dict[str, str]
+                for emote in bttvData['emotes']:  # --type: Dict[str, str]
+                    emotes[emote['id']] = emote['code']
+                return emotes
+    except HTTPError as e:
+        if e.code == 404:
+            return {}
+    except URLError:
+        pass
     return None
--- source/api/ffz.py
+++ source/api/ffz.py
@@ -1,8 +1,7 @@
 ﻿import json
 from http.client import HTTPResponse
-from contextlib import suppress
 from typing import Dict, Optional, Union
-from urllib.error import URLError
+from urllib.error import HTTPError, URLError
 from urllib.request import urlopen
 
 FfzEmoteDict = Dict[str, Union[int, str]]
@@ -10,31 +9,43 @@ FfzEmoteDict = Dict[str, Union[int, str]]
 
 def getGlobalEmotes() -> Optional[Dict[int, str]]:
     url = 'https://api.frankerfacez.com/v1/set/global'
-    with suppress(URLError), urlopen(url) as response:
-        if not isinstance(response, HTTPResponse):
-            raise TypeError()
-        if response.status == 200:
-            responseData = response.read()  # type: bytes
-            ffzData = json.loads(responseData.decode())  # type: dict
-            emotes = {}  # type: Dict[int, str]
-            for s in ffzData['default_sets']:  # --type: str
-                for emote in ffzData['sets'][str(s)]['emoticons']:  # --type: FfzEmoteDict
-                    emotes[emote['id']] = emote['name']
-            return emotes
+    try:
+        with urlopen(url) as response:
+            if not isinstance(response, HTTPResponse):
+                raise TypeError()
+            if response.status == 200:
+                responseData = response.read()  # type: bytes
+                ffzData = json.loads(responseData.decode())  # type: dict
+                emotes = {}  # type: Dict[int, str]
+                for s in ffzData['default_sets']:  # --type: str
+                    for emote in ffzData['sets'][str(s)]['emoticons']:  # --type: FfzEmoteDict
+                        emotes[emote['id']] = emote['name']
+                return emotes
+    except HTTPError as e:
+        if e.code == 404:
+            return {}
+    except URLError:
+        pass
     return None
 
 
 def getBroadcasterEmotes(broadcaster: str) -> Optional[Dict[int, str]]:
     url = 'https://api.frankerfacez.com/v1/room/' + broadcaster
-    with suppress(URLError), urlopen(url) as response:
-        if not isinstance(response, HTTPResponse):
-            raise TypeError()
-        if response.status == 200:
-            responseData = response.read()  # type: bytes
-            ffzData = json.loads(responseData.decode())  # type: dict
-            emotes = {}
-            ffzSet = ffzData['room']['set']  # type: str
-            for emote in ffzData['sets'][str(ffzSet)]['emoticons']:  # --type: FfzEmoteDict
-                emotes[emote['id']] = emote['name']
-            return emotes
+    try:
+        with urlopen(url) as response:
+            if not isinstance(response, HTTPResponse):
+                raise TypeError()
+            if response.status == 200:
+                responseData = response.read()  # type: bytes
+                ffzData = json.loads(responseData.decode())  # type: dict
+                emotes = {}
+                ffzSet = ffzData['room']['set']  # type: str
+                for emote in ffzData['sets'][str(ffzSet)]['emoticons']:  # --type: FfzEmoteDict
+                    emotes[emote['id']] = emote['name']
+                return emotes
+    except HTTPError as e:
+        if e.code == 404:
+            return {}
+    except URLError:
+        pass
     return None
