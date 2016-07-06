6c1a53e70a9653bcf64329b770e1c2870df027a0
MeGotsThis <megotsthis@gmail.com>
2016-07-05 17:41:27 -0700

    Added unit tests for source/api

---
+++ unittests/public/test_api.py
@@ -0,0 +1,51 @@
+import unittest
+from datetime import datetime, timedelta
+from source import api
+from unittest.mock import patch
+
+
+class TestApi(unittest.TestCase):
+    @patch('source.api.utils.now', autospec=True)
+    @patch('source.api.globals', autospec=True)
+    def test_cache(self, mock_globals, mock_now):
+        basenow = datetime(2000, 1, 1)
+        data = {}
+        mock_now.return_value = basenow
+        mock_globals.globalSessionData = data
+        i = 0
+
+        @api.cache('Kappa', timedelta(seconds=60))
+        def d():
+            nonlocal i
+            i += 1
+            return i
+
+        self.assertIn('Kappa', mock_globals.globalSessionData)
+        self.assertEqual(d(), 1)
+        self.assertIn(((), ()), mock_globals.globalSessionData['Kappa'])
+        self.assertEqual(
+            basenow, mock_globals.globalSessionData['Kappa'][(), ()][0])
+        self.assertEqual(mock_globals.globalSessionData['Kappa'][(), ()][1], 1)
+        mock_now.return_value = basenow + timedelta(seconds=30)
+        self.assertEqual(d(), 1)
+        self.assertEqual(
+            basenow, mock_globals.globalSessionData['Kappa'][(), ()][0])
+        self.assertEqual(mock_globals.globalSessionData['Kappa'][(), ()][1], 1)
+        mock_now.return_value = basenow + timedelta(seconds=60)
+        self.assertEqual(d(), 2)
+        self.assertEqual(
+            basenow + timedelta(seconds=60),
+            mock_globals.globalSessionData['Kappa'][(), ()][0])
+        self.assertEqual(mock_globals.globalSessionData['Kappa'][(), ()][1], 2)
+
+    @patch('source.api.utils.now', autospec=True)
+    @patch('source.api.globals', autospec=True)
+    def test_cache_except(self, mock_globals, mock_now):
+        mock_now.return_value = datetime(2000, 1, 1)
+        mock_globals.globalSessionData = {}
+
+        @api.cache('Kappa', timedelta(seconds=60))
+        def d():
+            raise ConnectionError()
+
+        self.assertEqual(d(), None)
