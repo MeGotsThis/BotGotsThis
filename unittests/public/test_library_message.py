986806e432d44fe2305b6c36e3cdacb3a45d9427
MeGotsThis <megotsthis@gmail.com>
2016-07-15 00:19:37 -0700

    Added unit tests for source/public/library/message

---
+++ unittests/public/test_library_message.py
@@ -0,0 +1,51 @@
+import unittest
+from source.public.library import message
+from unittest.mock import patch
+
+
+def send(messages):
+    pass
+
+
+def method(args):
+    return True
+
+
+class TestLibraryMessage(unittest.TestCase):
+    def setUp(self):
+        patcher = patch('source.public.library.message.config')
+        self.addCleanup(patcher.stop)
+        self.mock_config = patcher.start()
+        self.mock_config.messageLimit = 30
+
+    def test(self):
+        self.assertEqual(list(message.messagesFromItems([])), [])
+
+    def test_one_item(self):
+        self.assertEqual(list(message.messagesFromItems(['a'])), ['a'])
+
+    def test_two_items(self):
+        self.assertEqual(
+            list(message.messagesFromItems(['a', 'b'])), ['a, b'])
+
+    def test_too_many_items(self):
+        self.assertEqual(
+            list(message.messagesFromItems(['a'] * 11)),
+            ['a, ' * 9 + 'a', 'a'])
+
+    def test_too_many_many_items(self):
+        self.assertEqual(
+            list(message.messagesFromItems(['a'] * 31)),
+            ['a, ' * 9 + 'a'] * 3 + ['a'])
+
+    def test_prepend(self):
+        self.assertEqual(list(message.messagesFromItems([], 'Items: ')), [])
+
+    def test_prepend_one_item(self):
+        self.assertEqual(
+            list(message.messagesFromItems(['a'], 'Items: ')), ['Items: a'])
+
+    def test_prepend_too_many_items(self):
+        self.assertEqual(
+            list(message.messagesFromItems(['a'] * 9, 'Items: ')),
+            ['Items: ' + 'a, ' * 7 + 'a', 'Items: a'])
