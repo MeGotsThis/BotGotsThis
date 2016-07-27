59863f85227227c3a4537ee821a3f8bb7a116327
MeGotsThis <megotsthis@gmail.com>
2016-07-27 13:13:35 -0700

    Added unit tests for source/public/whisper/feature

---
+++ unittests/public/test_whisper_feature.py
@@ -0,0 +1,17 @@
+from source.data.message import Message
+from source.public.whisper import feature
+from unittest.mock import ANY, patch
+from unittests.public.test_whisper import TestWhisper
+
+
+class TestWhisperFeature(TestWhisper):
+    @patch('source.public.whisper.feature.feature.feature', autospec=True)
+    def test_feature(self, mock_feature):
+        self.assertIs(feature.commandFeature(self.args), False)
+        self.assertFalse(mock_feature.called)
+        mock_feature.return_value = True
+        message = Message('!feature feature')
+        self.assertIs(
+            feature.commandFeature(self.args._replace(message=message)), True)
+        mock_feature.assert_called_once_with(self.database, 'botgotsthis',
+                                             message, ANY)
