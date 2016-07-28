e5f28f7b0ac951dae0aa921c6050d71e27739748
MeGotsThis <megotsthis@gmail.com>
2016-07-28 15:59:44 -0700

    Added unit tests for source/public/custom/query

--- source/public/custom/query.py
+++ source/public/custom/query.py
@@ -5,7 +5,9 @@ from ...data import CustomFieldArgs
 def fieldQuery(args: CustomFieldArgs) -> Optional[str]:
     if args.field.lower() == 'query':
         if len(args.message) > 1:
-            return args.prefix + args.message.query + args.suffix
+            prefix = args.prefix or ''  # type: str
+            suffix = args.suffix or ''  # type: str
+            return prefix + args.message.query + suffix
         else:
-            return args.default
+            return args.default or ''
     return None
---
+++ unittests/public/test_custom_query.py
@@ -0,0 +1,41 @@
+from source.data.message import Message
+from source.public.custom import query
+from unittests.public.test_custom import TestCustomField
+
+
+class TestCustomQuery(TestCustomField):
+    def setUp(self):
+        super().setUp()
+        self.args = self.args._replace(field='query', message=Message('a b c'))
+
+    def test(self):
+        self.args = self.args._replace(field='')
+        self.assertIsNone(query.fieldQuery(self.args))
+
+    def test_query(self):
+        self.assertEqual(query.fieldQuery(self.args), 'b c')
+
+    def test_caps(self):
+        self.args = self.args._replace(field='QUERY')
+        self.assertEqual(query.fieldQuery(self.args), 'b c')
+
+    def test_default(self):
+        self.args = self.args._replace(message=Message(''),
+                                       prefix='[', suffix=']')
+        self.assertEqual(query.fieldQuery(self.args), '')
+
+    def test_prefix(self):
+        self.args = self.args._replace(prefix='[')
+        self.assertEqual(query.fieldQuery(self.args), '[b c')
+
+    def test_prefix_blank(self):
+        self.args = self.args._replace(prefix='')
+        self.assertEqual(query.fieldQuery(self.args), 'b c')
+
+    def test_suffix(self):
+        self.args = self.args._replace(suffix=']')
+        self.assertEqual(query.fieldQuery(self.args), 'b c]')
+
+    def test_suffix_blank(self):
+        self.args = self.args._replace(suffix='')
+        self.assertEqual(query.fieldQuery(self.args), 'b c')
