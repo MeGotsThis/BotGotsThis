35390fb15502540373336f83f9c9b5b86fe801aa
MeGotsThis <megotsthis@gmail.com>
2016-07-29 11:10:40 -0700

    Added unit tests for source/public/custom/params

--- source/public/custom/params.py
+++ source/public/custom/params.py
@@ -8,25 +8,28 @@ def fieldParams(args: CustomFieldArgs) -> Optional[str]:
     with suppress(TypeError):
         match = re.fullmatch(r'(\d+)(-(\d+))?|(\d+)-|-(\d+)', args.field)  # type: Match[str]
         if match is not None:
+            prefix = args.prefix or ''
+            suffix = args.suffix or ''
             matchParts = match.groups()  # type: Sequence[str]
             start = None  # type: Optional[int]
             stop = None  # type: Optional[int]
             if matchParts[0] is not None:
                 start = int(matchParts[0])
                 if matchParts[2] is not None:
-                    stop = int(matchParts[2])
+                    stop = int(matchParts[2]) + 1
                 else:
                     if start < len(args.message):
-                        return args.prefix + args.message[start] + args.suffix
+                        return prefix + args.message[start] + suffix
                     else:
-                        return args.default
+                        return args.default or ''
             elif matchParts[3] is not None:
                 start = int(matchParts[3])
             elif matchParts[4] is not None:
-                stop = int(matchParts[4])
+                start = 1
+                stop = int(matchParts[4]) + 1
             params = args.message[start:stop]
             if params:
-                return args.prefix + params + args.suffix
+                return prefix + params + suffix
             else:
-                return args.default
+                return args.default or ''
     return None
---
+++ unittests/public/test_custom_params.py
@@ -0,0 +1,58 @@
+from source.data.message import Message
+from source.public.custom import params
+from unittests.public.test_custom import TestCustomField
+
+
+class TestCustomParams(TestCustomField):
+    def setUp(self):
+        super().setUp()
+        self.args = self.args._replace(message=Message('a b c d e'))
+
+    def test(self):
+        self.args = self.args._replace(field='')
+        self.assertIsNone(params.fieldParams(self.args))
+
+    def test_zero(self):
+        self.args = self.args._replace(field='0')
+        self.assertEqual(params.fieldParams(self.args), 'a')
+
+    def test_one(self):
+        self.args = self.args._replace(field='1')
+        self.assertEqual(params.fieldParams(self.args), 'b')
+
+    def test_default(self):
+        self.args = self.args._replace(field='99',
+                                       prefix='[', suffix=']')
+        self.assertEqual(params.fieldParams(self.args), '')
+
+    def test_prefix(self):
+        self.args = self.args._replace(field='0', prefix='[')
+        self.assertEqual(params.fieldParams(self.args), '[a')
+
+    def test_prefix_blank(self):
+        self.args = self.args._replace(field='0', prefix='')
+        self.assertEqual(params.fieldParams(self.args), 'a')
+
+    def test_suffix(self):
+        self.args = self.args._replace(field='0', suffix=']')
+        self.assertEqual(params.fieldParams(self.args), 'a]')
+
+    def test_suffix_blank(self):
+        self.args = self.args._replace(field='0', suffix='')
+        self.assertEqual(params.fieldParams(self.args), 'a')
+
+    def test_range(self):
+        self.args = self.args._replace(field='1-2')
+        self.assertEqual(params.fieldParams(self.args), 'b c')
+        self.args = self.args._replace(field='0-2')
+        self.assertEqual(params.fieldParams(self.args), 'a b c')
+
+    def test_starting(self):
+        self.args = self.args._replace(field='1-')
+        self.assertEqual(params.fieldParams(self.args), 'b c d e')
+        self.args = self.args._replace(field='0-')
+        self.assertEqual(params.fieldParams(self.args), 'a b c d e')
+
+    def test_ending(self):
+        self.args = self.args._replace(field='-2')
+        self.assertEqual(params.fieldParams(self.args), 'b c')
