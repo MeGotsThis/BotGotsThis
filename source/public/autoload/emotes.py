0dc140b4e0f24ae620b651e5d9abe84b65cfd609
MeGotsThis <megotsthis@gmail.com>
2016-08-08 11:17:45 -0700

    Changed to indirect reference of the tasks

--- source/public/autoload/emotes.py
+++ source/public/autoload/emotes.py
@@ -2,9 +2,19 @@
 from bot.globals import background
 import datetime
 
-background.addTask(emotes.refreshTwitchGlobalEmotes,
-                   datetime.timedelta(seconds=1))
-background.addTask(emotes.refreshFrankerFaceZEmotes,
-                   datetime.timedelta(milliseconds=.75))
-background.addTask(emotes.refreshBetterTwitchTvEmotes,
-                   datetime.timedelta(milliseconds=.75))
+
+def call_twitch(timestamp: datetime.datetime) -> None:
+    emotes.refreshTwitchGlobalEmotes(timestamp)
+
+
+def call_ffz(timestamp: datetime.datetime) -> None:
+    emotes.refreshFrankerFaceZEmotes(timestamp)
+
+
+def call_bttv(timestamp: datetime.datetime) -> None:
+    emotes.refreshBetterTwitchTvEmotes(timestamp)
+
+
+background.addTask(call_twitch, datetime.timedelta(seconds=1))
+background.addTask(call_ffz, datetime.timedelta(milliseconds=.75))
+background.addTask(call_bttv, datetime.timedelta(milliseconds=.75))
