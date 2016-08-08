a74f44021f7231cec7200730782c3676af7b2912
MeGotsThis <megotsthis@gmail.com>
2016-08-08 11:28:10 -0700

    More indirect references

--- source/public/autoload/twitch.py
+++ source/public/autoload/twitch.py
@@ -3,12 +3,22 @@ from bot.globals import background
 import datetime
 import threading
 
+
+def call_streams(timestamp: datetime.datetime) -> None:
+    twitch.checkStreamsAndChannel(timestamp)
+
+
+def call_offline(timestamp: datetime.datetime) -> None:
+    twitch.checkOfflineChannels(timestamp)
+
+
+def call_server(timestamp: datetime.datetime) -> None:
+    twitch.checkChatServers(timestamp)
+
+
 threading.Timer(1, background.addTask, 
-                (twitch.checkStreamsAndChannel,
-                 datetime.timedelta(seconds=30))).start()
+                (call_streams, datetime.timedelta(seconds=30))).start()
 threading.Timer(10, background.addTask,
-                (twitch.checkOfflineChannels,
-                 datetime.timedelta(seconds=0.05))).start()
+                (call_offline, datetime.timedelta(seconds=0.05))).start()
 threading.Timer(10, background.addTask,
-                (twitch.checkChatServers,
-                 datetime.timedelta(seconds=0.05))).start()
+                (call_server, datetime.timedelta(seconds=0.05))).start()
