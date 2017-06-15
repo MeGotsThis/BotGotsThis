33a5117c04e97427769c023971b1fc8758179884
MeGotsThis <megotsthis@gmail.com>
2017-06-14 17:59:33 -0700

    Added raise_for_status in aiohttp

--- source/api/bttv.py
+++ source/api/bttv.py
@@ -11,7 +11,7 @@ async def getGlobalEmotes() -> Optional[Dict[str, str]]:
     url: str = 'https://api.betterttv.net/2/emotes'
     response: aiohttp.ClientResponse
     try:
-        async with aiohttp.ClientSession() as session:
+        async with aiohttp.ClientSession(raise_for_status=True) as session:
             async with session.get(url,
                                    timeout=config.httpTimeout) as response:
                 if response.status != 200:
@@ -34,7 +34,7 @@ async def getBroadcasterEmotes(broadcaster: str) -> Optional[Dict[str, str]]:
     url: str = 'https://api.betterttv.net/2/channels/' + broadcaster
     response: aiohttp.ClientResponse
     try:
-        async with aiohttp.ClientSession() as session:
+        async with aiohttp.ClientSession(raise_for_status=True) as session:
             async with session.get(url,
                                    timeout=config.httpTimeout) as response:
                 if response.status != 200:
