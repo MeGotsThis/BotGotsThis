1306c2e94c30704344aa7d2d5acab362761945bf
MeGotsThis <megotsthis@gmail.com>
2017-06-16 15:11:09 -0700

    Converted source/public/whisper/broadcaster.py to async

--- source/public/whisper/broadcaster.py
+++ source/public/whisper/broadcaster.py
@@ -3,19 +3,19 @@ from ..library.whisper import send
 from ...data import WhisperCommandArgs
 
 
-def commandCome(args: WhisperCommandArgs) -> bool:
-    return broadcaster.come(args.database, args.nick, send(args.nick))
+async def commandCome(args: WhisperCommandArgs) -> bool:
+    return await broadcaster.come(args.database, args.nick, send(args.nick))
 
 
-def commandLeave(args: WhisperCommandArgs) -> bool:
+async def commandLeave(args: WhisperCommandArgs) -> bool:
     return broadcaster.leave(args.nick, send(args.nick))
 
 
-def commandEmpty(args: WhisperCommandArgs) -> bool:
+async def commandEmpty(args: WhisperCommandArgs) -> bool:
     return broadcaster.empty(args.nick, send(args.nick))
 
 
-def commandAutoJoin(args: WhisperCommandArgs) -> bool:
-    return broadcaster.auto_join(args.database, args.nick, send(args.nick),
-                                 args.message)
+async def commandAutoJoin(args: WhisperCommandArgs) -> bool:
+    return await broadcaster.auto_join(args.database, args.nick,
+                                       send(args.nick), args.message)
 
