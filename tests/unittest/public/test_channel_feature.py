8748f8182963d8bb6a98ce82af9a46746f52c6c1
MeGotsThis <megotsthis@gmail.com>
2017-06-16 10:18:47 -0700

    Converted source/public/channel/feature.py to async

--- tests/unittest/public/test_channel_feature.py
+++ tests/unittest/public/test_channel_feature.py
@@ -1,20 +1,22 @@
-from unittest.mock import patch
+from asynctest.mock import patch
 
 from source.data.message import Message
-from source.public.channel import feature
 from tests.unittest.base_channel import TestChannel
+
+# Needs to be imported last
+from source.public.channel import feature
 
 
 class TestChannelFeature(TestChannel):
-    @patch('source.public.library.feature.feature', autospec=True)
-    def test_feature(self, mock_feature):
-        self.assertIs(feature.commandFeature(self.args), False)
+    @patch('source.public.library.feature.feature')
+    async def test_feature(self, mock_feature):
+        self.assertIs(await feature.commandFeature(self.args), False)
         self.assertFalse(mock_feature.called)
         mock_feature.return_value = True
         self.permissions.inOwnerChannel = True
         self.permissionSet['broadcaster'] = True
         message = Message('!feature feature')
-        self.assertIs(
-            feature.commandFeature(self.args._replace(message=message)), True)
+        args = self.args._replace(message=message)
+        self.assertIs(await feature.commandFeature(args), True)
         mock_feature.assert_called_once_with(self.database, 'botgotsthis',
                                              message, self.channel.send)
