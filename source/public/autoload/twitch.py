12122336a2dfc34d6749953fc4ad7a00f4f6a06c
MeGotsThis <megotsthis@gmail.com>
2017-06-15 22:27:17 -0700

    Converted threading to asyncio

--- source/public/autoload/twitch.py
+++ source/public/autoload/twitch.py
@@ -1,6 +1,5 @@
 ﻿import asyncio
 import datetime
-import threading
 
 from bot.coroutine import background
 from ..tasks import twitch
@@ -22,11 +21,12 @@ async def call_server(timestamp: datetime.datetime) -> None:
     await twitch.checkChatServers(timestamp)
 
 
-threading.Timer(1.5, background.add_task,
-                [call_ids, datetime.timedelta(seconds=10)]).start()
-threading.Timer(5, background.add_task,
-                [call_streams, datetime.timedelta(seconds=30)]).start()
-threading.Timer(10, background.add_task,
-                [call_offline, datetime.timedelta(seconds=0.05)]).start()
-threading.Timer(10, background.add_task,
-                [call_server, datetime.timedelta(seconds=0.05)]).start()
+loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
+loop.call_later(1.5, background.add_task, call_ids,
+                datetime.timedelta(seconds=10))
+loop.call_later(5, background.add_task, call_streams,
+                datetime.timedelta(seconds=30))
+loop.call_later(10, background.add_task, call_offline,
+                datetime.timedelta(seconds=0.05))
+loop.call_later(10, background.add_task, call_server,
+                datetime.timedelta(seconds=0.05))
