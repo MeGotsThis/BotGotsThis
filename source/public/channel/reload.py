71d326c343ab7a70dd25bcd249419e7b06dbb6a4
MeGotsThis <megotsthis@gmail.com>
2017-06-16 13:01:59 -0700

    Convert source/public/channel/reload.py to async

--- source/public/channel/reload.py
+++ source/public/channel/reload.py
@@ -5,17 +5,17 @@ from ...data import ChatCommandArgs
 
 @ownerChannel
 @permission('owner')
-def commandReload(args: ChatCommandArgs) -> bool:
+async def commandReload(args: ChatCommandArgs) -> bool:
     return reload.full_reload(sendPriority(args.chat, 0))
 
 
 @ownerChannel
 @permission('manager')
-def commandReloadCommands(args: ChatCommandArgs) -> bool:
+async def commandReloadCommands(args: ChatCommandArgs) -> bool:
     return reload.reload_commands(sendPriority(args.chat, 0))
 
 
 @ownerChannel
 @permission('owner')
-def commandReloadConfig(args: ChatCommandArgs) -> bool:
+async def commandReloadConfig(args: ChatCommandArgs) -> bool:
     return reload.reload_config(sendPriority(args.chat, 0))
