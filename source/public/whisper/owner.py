f8316870c58338a93067dba5ab25ec26f4d5d46d
MeGotsThis <megotsthis@gmail.com>
2017-06-16 15:07:48 -0700

    Ported source/public/whisper/owner.py to async

--- source/public/whisper/owner.py
+++ source/public/whisper/owner.py
@@ -5,46 +5,46 @@ from ...data import WhisperCommandArgs
 
 
 @permission('manager')
-def commandHello(args: WhisperCommandArgs) -> bool:
+async def commandHello(args: WhisperCommandArgs) -> bool:
     utils.whisper(args.nick, 'Hello Kappa !')
     return True
 
 
 @permission('owner')
-def commandExit(args: WhisperCommandArgs) -> bool:
+async def commandExit(args: WhisperCommandArgs) -> bool:
     return exit.exit(send(args.nick))
 
 
 @permission('manager')
-def commandSay(args: WhisperCommandArgs) -> bool:
+async def commandSay(args: WhisperCommandArgs) -> bool:
     return channel.say(args.database, args.nick, args.message.lower[1],
                        args.message[2:])
 
 
 @min_args(2)
-def commandJoin(args: WhisperCommandArgs) -> bool:
-    return channel.join(args.database, args.message.lower[1],
-                        send(args.nick))
+async def commandJoin(args: WhisperCommandArgs) -> bool:
+    return await channel.join(args.database, args.message.lower[1],
+                              send(args.nick))
 
 
 @min_args(2)
-def commandPart(args: WhisperCommandArgs) -> bool:
+async def commandPart(args: WhisperCommandArgs) -> bool:
     return channel.part(args.message.lower[1], send(args.nick))
 
 
 @permission('admin')
-def commandEmptyAll(args: WhisperCommandArgs) -> bool:
+async def commandEmptyAll(args: WhisperCommandArgs) -> bool:
     return channel.empty_all(send(args.nick))
 
 
 @min_args(2)
 @permission('admin')
-def commandEmpty(args: WhisperCommandArgs) -> bool:
+async def commandEmpty(args: WhisperCommandArgs) -> bool:
     return channel.empty(args.message.lower[1], send(args.nick))
 
 
 @min_args(2)
 @permission('manager')
-def commandManageBot(args: WhisperCommandArgs) -> bool:
-    return managebot.manage_bot(args.database, args.permissions,
-                                send(args.nick), args.nick, args.message)
+async def commandManageBot(args: WhisperCommandArgs) -> bool:
+    return await managebot.manage_bot(args.database, args.permissions,
+                                      send(args.nick), args.nick, args.message)
