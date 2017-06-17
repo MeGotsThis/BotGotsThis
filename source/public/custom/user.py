a81693336a99141febfffa9a778cba3f63eb4875
MeGotsThis <megotsthis@gmail.com>
2017-06-16 22:43:01 -0700

    Converted source/public/custom/user.py to async

--- source/public/custom/user.py
+++ source/public/custom/user.py
@@ -2,7 +2,7 @@
 from ...data import CustomFieldArgs
 
 
-def fieldUser(args: CustomFieldArgs) -> Optional[str]:
+async def fieldUser(args: CustomFieldArgs) -> Optional[str]:
     if args.field.lower() == 'user' or args.field.lower() == 'nick':
         if args.nick:
             return (args.prefix or '') + args.nick + (args.suffix or '')
