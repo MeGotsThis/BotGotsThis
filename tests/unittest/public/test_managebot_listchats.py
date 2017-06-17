c8d451bf5d6d744b23e08f4ab9a4805463864b30
MeGotsThis <megotsthis@gmail.com>
2017-06-16 21:40:58 -0700

    Converted source/public/manage/listchats.py to async

--- tests/unittest/public/test_managebot_listchats.py
+++ tests/unittest/public/test_managebot_listchats.py
@@ -1,37 +1,41 @@
-from unittest.mock import patch
+import lists.manage
 
-from source.public.manage import listchats
+from asynctest.mock import patch
+
 from tests.unittest.base_managebot import TestManageBot
 from tests.unittest.mock_class import StrContains
+
+# Needs to be imported last
+from source.public.manage import listchats
 
 
 class TestManageBotListChats(TestManageBot):
     @patch('source.public.library.message.messagesFromItems', autospec=True)
     @patch('bot.globals', autospec=True)
-    def test_no_channels(self, mock_globals, mock_messages):
+    async def test_no_channels(self, mock_globals, mock_messages):
         mock_globals.channels = ''
-        self.assertIs(listchats.manageListChats(self.args), True)
+        self.assertIs(await listchats.manageListChats(self.args), True)
         self.assertFalse(mock_messages.called)
         self.send.assert_called_once_with(StrContains('not', 'in'))
 
     @patch('source.public.library.message.messagesFromItems', autospec=True)
     @patch('bot.globals', autospec=True)
-    def test_one_channel(self, mock_globals, mock_messages):
+    async def test_one_channel(self, mock_globals, mock_messages):
         mock_globals.channels = {'botgotsthis': None}
         mock_messages.return_value = ''
-        self.assertIs(listchats.manageListChats(self.args), True)
+        self.assertIs(await listchats.manageListChats(self.args), True)
         mock_messages.assert_called_once_with(['botgotsthis'],
                                               StrContains('Chats'))
         self.send.assert_called_once_with('')
 
     @patch('source.public.library.message.messagesFromItems', autospec=True)
     @patch('bot.globals', autospec=True)
-    def test_many_channel(self, mock_globals, mock_messages):
+    async def test_many_channel(self, mock_globals, mock_messages):
         mock_globals.channels = {'botgotsthis': None,
                                  'mebotsthis': None,
                                  'megotsthis': None}
         mock_messages.return_value = ''
-        self.assertIs(listchats.manageListChats(self.args), True)
+        self.assertIs(await listchats.manageListChats(self.args), True)
         mock_messages.assert_called_once_with(
             ['botgotsthis', 'mebotsthis', 'megotsthis'], StrContains('Chats'))
         self.send.assert_called_once_with('')
