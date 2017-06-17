4fd5fad5fb6056d93b5f7c91a783da4395d7726b
MeGotsThis <megotsthis@gmail.com>
2017-06-16 22:43:01 -0700

    Converted source/public/custom/user.py to async

--- tests/unittest/public/test_custom_user.py
+++ tests/unittest/public/test_custom_user.py
@@ -1,5 +1,7 @@
-from source.public.custom import user
 from tests.unittest.base_custom import TestCustomField
+
+# Needs to be imported last
+from source.public.custom import user
 
 
 class TestCustomUser(TestCustomField):
@@ -7,41 +9,41 @@ class TestCustomUser(TestCustomField):
         super().setUp()
         self.args = self.args._replace(field='user', nick='megotsthis')
 
-    def test(self):
+    async def test(self):
         self.args = self.args._replace(field='')
-        self.assertIsNone(user.fieldUser(self.args))
+        self.assertIsNone(await user.fieldUser(self.args))
 
-    def test_user(self):
-        self.assertEqual(user.fieldUser(self.args), 'megotsthis')
+    async def test_user(self):
+        self.assertEqual(await user.fieldUser(self.args), 'megotsthis')
 
-    def test_nick(self):
+    async def test_nick(self):
         self.args = self.args._replace(field='nick')
-        self.assertEqual(user.fieldUser(self.args), 'megotsthis')
+        self.assertEqual(await user.fieldUser(self.args), 'megotsthis')
 
-    def test_user_caps(self):
+    async def test_user_caps(self):
         self.args = self.args._replace(field='USER')
-        self.assertEqual(user.fieldUser(self.args), 'megotsthis')
+        self.assertEqual(await user.fieldUser(self.args), 'megotsthis')
 
-    def test_nick_caps(self):
+    async def test_nick_caps(self):
         self.args = self.args._replace(field='NICK')
-        self.assertEqual(user.fieldUser(self.args), 'megotsthis')
+        self.assertEqual(await user.fieldUser(self.args), 'megotsthis')
 
-    def test_default(self):
+    async def test_default(self):
         self.args = self.args._replace(nick='', prefix='[', suffix=']')
-        self.assertEqual(user.fieldUser(self.args), '')
+        self.assertEqual(await user.fieldUser(self.args), '')
 
-    def test_prefix(self):
+    async def test_prefix(self):
         self.args = self.args._replace(prefix='[')
-        self.assertEqual(user.fieldUser(self.args), '[megotsthis')
+        self.assertEqual(await user.fieldUser(self.args), '[megotsthis')
 
-    def test_prefix_blank(self):
+    async def test_prefix_blank(self):
         self.args = self.args._replace(prefix='')
-        self.assertEqual(user.fieldUser(self.args), 'megotsthis')
+        self.assertEqual(await user.fieldUser(self.args), 'megotsthis')
 
-    def test_suffix(self):
+    async def test_suffix(self):
         self.args = self.args._replace(suffix=']')
-        self.assertEqual(user.fieldUser(self.args), 'megotsthis]')
+        self.assertEqual(await user.fieldUser(self.args), 'megotsthis]')
 
-    def test_suffix_blank(self):
+    async def test_suffix_blank(self):
         self.args = self.args._replace(suffix='')
-        self.assertEqual(user.fieldUser(self.args), 'megotsthis')
+        self.assertEqual(await user.fieldUser(self.args), 'megotsthis')
