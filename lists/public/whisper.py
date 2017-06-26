22726f8466fb7a1ed1648c6e85b016da6f7b0815
MeGotsThis <megotsthis@gmail.com>
2017-06-26 14:02:02 -0700

    Converted static variables to functions in lists/whisper.py

--- lists/public/whisper.py
+++ lists/public/whisper.py
@@ -2,21 +2,25 @@
 from source.public.whisper import broadcaster, feature, owner, reload
 from typing import Mapping, Optional
 
-commands: Mapping[str, Optional[data.WhisperCommand]] = {
-    '!hello': owner.commandHello,
-    '!exit': owner.commandExit,
-    '!say': owner.commandSay,
-    '!join': owner.commandJoin,
-    '!part': owner.commandPart,
-    '!emptychat': owner.commandEmpty,
-    '!emptyall': owner.commandEmptyAll,
-    '!managebot': owner.commandManageBot,
-    '!reload': reload.commandReload,
-    '!reloadcommands': reload.commandReloadCommands,
-    '!reloadconfig': reload.commandReloadConfig,
-    '!leave': broadcaster.commandLeave,
-    '!empty': broadcaster.commandEmpty,
-    '!come': broadcaster.commandCome,
-    '!autojoin': broadcaster.commandAutoJoin,
-    '!feature': feature.commandFeature,
-    }
+
+def commands() -> Mapping[str, Optional[data.WhisperCommand]]:
+    if not hasattr(commands, 'commands'):
+        setattr(commands, 'commands', {
+            '!hello': owner.commandHello,
+            '!exit': owner.commandExit,
+            '!say': owner.commandSay,
+            '!join': owner.commandJoin,
+            '!part': owner.commandPart,
+            '!emptychat': owner.commandEmpty,
+            '!emptyall': owner.commandEmptyAll,
+            '!managebot': owner.commandManageBot,
+            '!reload': reload.commandReload,
+            '!reloadcommands': reload.commandReloadCommands,
+            '!reloadconfig': reload.commandReloadConfig,
+            '!leave': broadcaster.commandLeave,
+            '!empty': broadcaster.commandEmpty,
+            '!come': broadcaster.commandCome,
+            '!autojoin': broadcaster.commandAutoJoin,
+            '!feature': feature.commandFeature,
+            })
+    return getattr(commands, 'commands')
