75cb11ec78110814b2c0c0768dbdd1be47b03aeb
MeGotsThis <megotsthis@gmail.com>
2017-06-27 00:29:56 -0700

    Fixed for flake8

--- lists/private/whisper.dist.disableAll.py
+++ lists/private/whisper.dist.disableAll.py
@@ -5,22 +5,22 @@ from typing import Mapping, Optional
 def commands() -> Mapping[str, Optional[WhisperCommand]]:
     if not hasattr(commands, 'commands'):
         setattr(commands, 'commands', {
-                '!hello': None,
-                '!exit': None,
-                '!say': None,
-                '!join': None,
-                '!part': None,
-                '!emptychat': None,
-                '!emptyall': None,
-                '!managebot': None,
-                '!reload': None,
-                '!reloadcommands': None,
-                '!reloadconfig': None,
-                '!leave': None,
-                '!empty': None,
-                '!come': None,
-                '!autojoin': None,
-                '!feature': None,
+            '!hello': None,
+            '!exit': None,
+            '!say': None,
+            '!join': None,
+            '!part': None,
+            '!emptychat': None,
+            '!emptyall': None,
+            '!managebot': None,
+            '!reload': None,
+            '!reloadcommands': None,
+            '!reloadconfig': None,
+            '!leave': None,
+            '!empty': None,
+            '!come': None,
+            '!autojoin': None,
+            '!feature': None,
             })
     return getattr(commands, 'commands')
 
