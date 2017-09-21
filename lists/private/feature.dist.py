2774b55067b60b5be7386d2509d0959181dae19d
MeGotsThis <megotsthis@gmail.com>
2017-09-21 10:56:56 -0700

    Removed all the private folders

--- lists/private/feature.dist.py
+++
@@ -1,5 +0,0 @@
-from typing import Mapping, Optional
-
-
-def features() -> Mapping[str, Optional[str]]:
-    return {}
--- source/private/ircmessage.dist.py
+++
@@ -1,8 +0,0 @@
-﻿from bot.coroutine import connection as connectionM  # noqa: F401
-from datetime import datetime
-
-
-def parseMessage(connection: 'connectionM.ConnectionHandler',
-                 ircmsg: str,
-                 now: datetime) -> None:
-    pass
