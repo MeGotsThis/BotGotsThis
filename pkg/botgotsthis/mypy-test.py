7746d641184c057ff294f7124f8e1003c9676176
MeGotsThis <megotsthis@gmail.com>
2017-09-21 14:22:47 -0700

    Added a mypy test for the botgotsthis pkg

---
+++ pkg/botgotsthis/mypy-test.py
@@ -0,0 +1,9 @@
+from .autoload import twitch  # noqa: F401
+from .autoload import emotes  # noqa: F401
+from .autoload import repeat  # noqa: F401
+
+from .items import channel  # noqa: F401
+from .items import whisper  # noqa: F401
+from .items import custom  # noqa: F401
+from .items import manage  # noqa: F401
+from .items import feature  # noqa: F401
