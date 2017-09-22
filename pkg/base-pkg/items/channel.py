96bf3bd6c7a509d31da801ef42d9d87003a35903
MeGotsThis <megotsthis@gmail.com>
2017-09-22 12:48:25 -0700

    Added a base-pkg

---
+++ pkg/base-pkg/items/channel.py
@@ -0,0 +1,19 @@
+﻿from typing import Iterable, Mapping, Optional
+
+from lib.data import ChatCommand
+
+
+def filterMessage() -> Iterable[ChatCommand]:
+    return []
+
+
+def commands() -> Mapping[str, Optional[ChatCommand]]:
+    return {}
+
+
+def commandsStartWith() -> Mapping[str, Optional[ChatCommand]]:
+    return {}
+
+
+def processNoCommand() -> Iterable[ChatCommand]:
+    return []
---
+++ pkg/base-pkg/items/feature.py
@@ -0,0 +1,26 @@
+﻿from typing import Mapping, Optional
+
+
+def features() -> Mapping[str, Optional[str]]:
+    if not hasattr(features, 'features'):
+        setattr(features, 'features', {
+            'helixfossil': 'Ask Helix Fossil',
+            'roll.exe': '!roll exe',
+            'bingo': 'Bingo Card',
+            'roll.emote': '!roll emotes',
+            'pokedex': 'Pokedex',
+            'noroll': 'Disable !roll',
+            'autopurge': 'Auto Purge',
+            'autobanword': 'Auto Ban Word',
+            'nosrlrace': 'Disable !srlrace',
+            'winnerbroadcaster': '!winner only for broadcaster',
+            'modstrawpoll': '!strawpoll for mods',
+            'noasciiart': 'No Large ASCII Art',
+            'nolinks': 'No URLs/Links',
+            'annoyinglinks': 'No Annoying URLs/Links',
+            'emotespam': 'No Emote Spam',
+            'quotes': 'Quotes',
+            'countdown': 'Countdown',
+            'modtree': 'Mod !tree'
+            })
+    return getattr(features, 'features')
