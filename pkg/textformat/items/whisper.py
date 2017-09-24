fed80c7aae4dc7a9936081288303d5f32d403a42
MeGotsThis <megotsthis@gmail.com>
2017-09-24 09:40:45 -0700

    Separated a repeat pkg from botgotsthis pkg

---
+++ pkg/textformat/items/whisper.py
@@ -0,0 +1,10 @@
+﻿from lib import data
+from typing import Mapping, Optional
+
+
+def commands() -> Mapping[str, Optional[data.WhisperCommand]]:
+    return {}
+
+
+def commandsStartWith() -> Mapping[str, Optional[data.WhisperCommand]]:
+    return {}
