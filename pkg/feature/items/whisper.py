51ab556e8f34a9d14cc093c2db8161a621ac1735
MeGotsThis <megotsthis@gmail.com>
2017-09-24 21:40:22 -0700

    Separated a feature pkg from botgotsthis pkg

---
+++ pkg/feature/items/whisper.py
@@ -0,0 +1,15 @@
+﻿from lib import data
+from .. import whisper
+from typing import Mapping, Optional
+
+
+def commands() -> Mapping[str, Optional[data.WhisperCommand]]:
+    if not hasattr(commands, 'commands'):
+        setattr(commands, 'commands', {
+            '!feature': whisper.commandFeature,
+            })
+    return getattr(commands, 'commands')
+
+
+def commandsStartWith() -> Mapping[str, Optional[data.WhisperCommand]]:
+    return {}
--- pkg/feature/whisper.py
+++ pkg/feature/whisper.py
@@ -1,9 +1,9 @@
 ﻿from lib.data import WhisperCommandArgs
 from lib.helper.whisper import min_args, send
-from ..library import feature
+from . import library
 
 
 @min_args(2)
 async def commandFeature(args: WhisperCommandArgs) -> bool:
-    return await feature.feature(args.database, args.nick, args.message,
+    return await library.feature(args.database, args.nick, args.message,
                                  send(args.nick))
