267d4f4a1220ae66518fc6ddb7058f4f51917a3f
MeGotsThis <megotsthis@gmail.com>
2017-11-03 23:46:26 -0700

    Separating features database functions to a separate class to help manage code

--- lib/database/main.py
+++ lib/database/main.py
@@ -9,6 +9,7 @@ from typing import overload
 from ._auto_join import AutoJoinMixin
 from ._base import Database
 from ._custom_commands import CustomCommandsMixin
+from ._features import FeaturesMixin
 from ._game_abbreviations import GameAbbreviationsMixin
 from .. import data
 
@@ -17,57 +18,7 @@ S = TypeVar('S')
 
 
 class DatabaseMain(AutoJoinMixin, GameAbbreviationsMixin, CustomCommandsMixin,
-                   Database):
-    def __init__(self,
-                 pool: aioodbc.Pool) -> None:
-        super().__init__(pool)
-        self.features: Optional[Set[str]] = None
-
-    async def getFeatures(self, broadcaster: str) -> AsyncIterator[str]:
-        query: str = '''
-SELECT feature FROM chat_features WHERE broadcaster=?
-'''
-        cursor: aioodbc.cursor.Cursor
-        async with await self.cursor() as cursor:
-            async for feature, in await cursor.execute(query, (broadcaster,)):
-                yield feature
-
-    async def hasFeature(self,
-                         broadcaster: str,
-                         feature: str) -> bool:
-        if self.features is None:
-            self.features = {f async for f in self.getFeatures(broadcaster)}
-        return feature in self.features
-
-    async def addFeature(self,
-                         broadcaster: str,
-                         feature: str) -> bool:
-        query: str = '''
-INSERT INTO chat_features (broadcaster, feature) VALUES (?, ?)
-'''
-        cursor: aioodbc.cursor.Cursor
-        async with await self.cursor() as cursor:
-            try:
-                await cursor.execute(query, (broadcaster, feature))
-                await self.commit()
-                self.features = None
-                return True
-            except pyodbc.Error:
-                return False
-
-    async def removeFeature(self,
-                            broadcaster: str,
-                            feature: str) -> bool:
-        query: str = '''
-DELETE FROM chat_features WHERE broadcaster=? AND feature=?
-'''
-        cursor: aioodbc.cursor.Cursor
-        async with await self.cursor() as cursor:
-            await cursor.execute(query, (broadcaster, feature))
-            self.features = None
-            await self.commit()
-            return cursor.rowcount != 0
-
+                   FeaturesMixin, Database):
     async def listBannedChannels(self) -> AsyncIterator[str]:
         query: str = '''SELECT broadcaster FROM banned_channels'''
         cursor: aioodbc.cursor.Cursor
