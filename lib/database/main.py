921a2ba6af4254b5c947fa0b5bb30d1fc39c5b61
MeGotsThis <megotsthis@gmail.com>
2017-11-03 23:49:00 -0700

    Separating banned channels database functions to a separate class to help manage code

--- lib/database/main.py
+++ lib/database/main.py
@@ -7,6 +7,7 @@ from typing import Optional, Sequence, Set, Tuple, TypeVar, Union  # noqa: F401
 from typing import overload
 
 from ._auto_join import AutoJoinMixin
+from ._banned_channels import BannedChannelsMixin
 from ._base import Database
 from ._custom_commands import CustomCommandsMixin
 from ._features import FeaturesMixin
@@ -18,73 +19,7 @@ S = TypeVar('S')
 
 
 class DatabaseMain(AutoJoinMixin, GameAbbreviationsMixin, CustomCommandsMixin,
-                   FeaturesMixin, Database):
-    async def listBannedChannels(self) -> AsyncIterator[str]:
-        query: str = '''SELECT broadcaster FROM banned_channels'''
-        cursor: aioodbc.cursor.Cursor
-        async with await self.cursor() as cursor:
-            broadcaster: str
-            async for broadcaster, in await cursor.execute(query):
-                yield broadcaster
-
-    async def isChannelBannedReason(self, broadcaster: str) -> Optional[str]:
-        query: str = '''
-SELECT reason FROM banned_channels WHERE broadcaster=?
-'''
-        cursor: aioodbc.cursor.Cursor
-        async with await self.cursor() as cursor:
-            await cursor.execute(query, (broadcaster,))
-            row: Optional[Tuple[str]] = await cursor.fetchone()
-            return row and row[0]
-
-    async def addBannedChannel(self,
-                               broadcaster: str,
-                               reason: str,
-                               nick: str) -> bool:
-        query: str = '''
-INSERT INTO banned_channels
-    (broadcaster, currentTime, reason, who)
-    VALUES (?, CURRENT_TIMESTAMP, ?, ?)
-'''
-        history: str = '''
-INSERT INTO banned_channels_log
-    (broadcaster, currentTime, reason, who, actionLog)
-    VALUES (?, CURRENT_TIMESTAMP, ?, ?, ?)
-'''
-        cursor: aioodbc.cursor.Cursor
-        async with await self.cursor() as cursor:
-            try:
-                await cursor.execute(query, (broadcaster, reason, nick))
-            except pyodbc.Error:
-                return False
-
-            await cursor.execute(history, (broadcaster, reason, nick, 'add'))
-            await self.commit()
-            return True
-
-    async def removeBannedChannel(self,
-                                  broadcaster: str,
-                                  reason: str,
-                                  nick: str) -> bool:
-        query: str = '''
-DELETE FROM banned_channels WHERE broadcaster=?
-'''
-        history: str = '''
-INSERT INTO banned_channels_log
-    (broadcaster, currentTime, reason, who, actionLog)
-    VALUES (?, CURRENT_TIMESTAMP, ?, ?, ?)
-'''
-        cursor: aioodbc.cursor.Cursor
-        async with await self.cursor() as cursor:
-            await cursor.execute(query, (broadcaster,))
-            if cursor.rowcount == 0:
-                return False
-
-            await cursor.execute(history, (broadcaster, reason, nick,
-                                           'remove'))
-            await self.commit()
-            return True
-
+                   FeaturesMixin, BannedChannelsMixin, Database):
     async def getAllChatProperties(self, broadcaster: str
                                    ) -> AsyncIterator[Tuple[str, str]]:
         query: str = '''
